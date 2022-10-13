4a3275d4b561eab711409cddaf38700fb0954324
abel (tudelft) <a.debruijn-3@student.tudelft.nl>
2022-10-13 21:43:20 +0200

    added tailwind to docs

--- apps/docs/src/routes/+page.svelte
+++ apps/docs/src/routes/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import { MyCounterButton } from 'ui';
 
-  import { Preview } from 'mdi-svelte-ts';
+  import { Canvas3D, Axis3D, Vector3D } from 'visuals-3d';
+
+  import { Vector3 } from 'three';
+  import { Label } from 'visuals-3d/utils/label';
+  // import { Preview } from 'mdi-svelte-ts';
 </script>
 
-<h1>Docs</h1>
+<h1 class="text-3xl font-bold underline">Docs</h1>
 <MyCounterButton />
+
+<Canvas3D --height="80vh">
+  <Vector3D direction="{new Vector3(1, 1, 0)}" label="{new Label('x')}" />
+
+  <Axis3D />
+</Canvas3D>
+
+<!-- <Preview /> -->
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
