08a3ea1719df7bad2a84a6073688fe231654b8d0
Abel de Bruijn <a.debruijn-3@student.tudelft.nl>
2022-10-16 14:05:40 +0200

    Update apps/docs/src/routes/3d-components/+page.ts
    minor change

--- apps/docs/src/routes/3d-components/+page.ts
+++ apps/docs/src/routes/3d-components/+page.ts
@@ -1,6 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
-// Redirect this route to /introduction
+// Redirect this route to /3d-components/canvas
 export function load() {
   throw redirect(302, '/3d-components/canvas');
 }
