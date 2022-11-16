fbce87b89c4795a02e2d3e72ddf7b7299c1b513d
Abel de Bruijn <abeldebruijn@hotmail.com>
2022-11-16 09:51:33 +0100

    setup threlte 5 with preprocessors

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,16 +1,16 @@
 import adapter from '@sveltejs/adapter-node';
 import { mdsvex } from 'mdsvex';
 
+import sequence from 'svelte-sequential-preprocessor';
 import preprocess from 'svelte-preprocess';
+import { preprocessThrelte } from '@threlte/preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: [
-    preprocess({
-      postcss: true
-    }),
+    sequence([preprocess({ postcss: true }), preprocessThrelte()]),
     mdsvex({
       extensions: ['.md', '.svx']
       // layout: 'src/routes/stories/layout.svelte'
--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-node';
+import sequence from 'svelte-sequential-preprocessor';
 import preprocess from 'svelte-preprocess';
+import { preprocessThrelte } from '@threlte/preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: sequence([preprocess(), preprocessThrelte()]),
 
   ssr: {
     noExternal: ['three', 'troika-three-text']
