30f939a8cee738c037f6fc688363770e8293b8ac
Abel de Bruijn <abeldebruijn@hotmail.com>
2022-11-20 16:17:05 +0100

    -

--- apps/docs/tailwind.config.cjs
+++ apps/docs/tailwind.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../../packages/ui/components/**/*.{html,js,svelte,ts,svx}',
+    '../../packages/ui/yan/**/*.{html,js,svelte,ts,svx}',
     '../../packages/visuals-3d/components/**/*.{html,js,svelte,ts,svx}',
     './src/**/*.{html,js,svelte,ts,svx}'
   ],
   theme: {
     extend: {}
   },
-  plugins: [require('@tailwindcss/typography')]
+  plugins: []
 };
