5f0eb41cf93dc22ec9ba5c86f5520d51d1188de6
Pauline <6712pauline@gmail.com>
2022-12-07 13:45:39 +0100

    Add dashedline
    and add old components to index of visuals-3d

--- packages/threlte-components/components/Line.svelte
+++ packages/threlte-components/components/Line.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
   import { T } from '@threlte/core';
-  import { BufferGeometry, Vector3 } from 'three';
+  import { BufferGeometry, LineDashedMaterial, Vector3 } from 'three';
 
   import getRandomColor from 'utils/PrimeColors';
 
   export let color = getRandomColor();
   export let points: [Vector3, Vector3] = [new Vector3(5, 0, 0), new Vector3(5, 0, 0)];
+  export let isDashed: boolean = false;
 
   $: geometry = new BufferGeometry().setFromPoints(points);
+
 </script>
 
 <T.Line {geometry}>
-  <T.LineBasicMaterial {color} />
+  {#if !isDashed}
+    <T.LineBasicMaterial {color} />
+  {:else}
+    <T.LineDashedMaterial 
+      {color} 
+      dashSize={ 0.1}
+      gapSize= {0.1}
+      opacity={ 0.5}
+      transparent={ true}
+    />
+  {/if}
 </T.Line>
