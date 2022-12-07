9ecfe7dcd434602d4c5485ddc6046ce99974f6f4
Pauline <6712pauline@gmail.com>
2022-12-07 12:21:03 +0100

    threlte component for Arc

--- packages/threlte-components/index.ts
+++ packages/threlte-components/index.ts
@@ -1,3 +1,4 @@
+export { default as Arc3D} from './components/Arc.svelte';
 export { default as Axis3D } from './components/Axis.svelte';
 export { default as Canvas3D } from './components/Canvas.svelte';
 export { default as Vector3D } from './components/Vector.svelte';
