e8af066541399129a17d3adc5471d512c93224b9
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-06-30 16:34:02 +0200

    fix plane from points

--- apps/web/src/lib/threlte-components/planes/PlaneFromPoints.svelte
+++ apps/web/src/lib/threlte-components/planes/PlaneFromPoints.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { T } from '@threlte/core';
+  import { T, useThrelte } from '@threlte/core';
   import { DoubleSide, Mesh, MeshBasicMaterial, Plane, PlaneGeometry, Vector3 } from 'three';
 
   import getRandomColor from 'utils/PrimeColors';
@@ -15,31 +15,37 @@
   export let opacity = 0.8;
   export let planeSegment = PlaneSegments.default();
 
-  function setStripes() {
-    geometry = new PlaneGeometry(size, size, planeSegment.segments, 1);
+  const { invalidate } = useThrelte();
 
-    for (let i = 0; i < planeSegment.segments; i++) {
-      if (i % planeSegment.interval == planeSegment.offset) {
-        // A rectangle consists of two triangles --> 6 vertices
-        geometry.addGroup(i * 6, 6, 0);
-      }
-    }
-  }
-
-  let geometry = new PlaneGeometry(size, size, planeSegment.segments, 1);
   let materials = [new MeshBasicMaterial({ color, transparent: true, opacity, side: DoubleSide })];
-
-  $: mesh = new Mesh(geometry, materials);
-
-  $: {
-    if (planeSegment.segments > 1) setStripes();
-    mesh.geometry = geometry;
-    mesh.material = materials;
-
-    const plane = new Plane().setFromCoplanarPoints(points[0], points[1], points[2]);
-    plane.coplanarPoint(mesh.position);
-    mesh.quaternion.setFromUnitVectors(new Vector3(0, 0, 1), plane.normal);
-  }
 </script>
 
-<T.Mesh bind:ref={mesh} />
+{#key points}
+  <T.Mesh
+    material={materials}
+    on:change={() => {
+      invalidate();
+    }}
+    on:create={({ ref, cleanup }) => {
+      const plane = new Plane().setFromCoplanarPoints(points[0], points[1], points[2]);
+      plane.coplanarPoint(ref.position);
+      ref.quaternion.setFromUnitVectors(new Vector3(0, 0, 1), plane.normal);
+
+      cleanup(() => {
+        materials.map((m) => m.dispose());
+      });
+    }}
+  >
+    <T.PlaneGeometry
+      args={[size, size, planeSegment.segments, 1]}
+      on:create={({ ref }) => {
+        for (let i = 0; i < planeSegment.segments; i++) {
+          if (i % planeSegment.interval == planeSegment.offset) {
+            // A rectangle consists of two triangles 6 vertices
+            ref.addGroup(i * 6, 6, 0);
+          }
+        }
+      }}
+    />
+  </T.Mesh>
+{/key}
