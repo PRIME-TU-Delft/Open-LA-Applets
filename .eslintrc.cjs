44db31ca5065f913df3dcbf9934df63027699c8b
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-07-27 16:33:49 +0200

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:svelte/recommended',
-    'prettier',
-    'plugin:storybook/recommended'
+    'plugin:storybook/recommended',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
