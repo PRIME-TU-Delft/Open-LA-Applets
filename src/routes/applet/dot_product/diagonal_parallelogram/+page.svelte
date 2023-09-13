b0105278bb999765fcda926cae8062023c976424
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-09-13 16:20:25 +0200

    Added innerProduct diagonal parallelogram

---
+++ src/routes/applet/dot_product/diagonal_parallelogram/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { Arc2D, Axis2D, Canvas2D, Latex2D, Vector2D } from '$lib/p5-components';
+
+  import { GridType } from '$lib/p5-components/components/Grids';
+  import { Vector2 } from 'three';
+  import { PrimeColor } from '$lib/utils/PrimeColors';
+  import Draggables from '$lib/p5-components/components/Draggables.svelte';
+
+  let w = new Vector2(2, 3);
+  const wDefault = new Vector2(2, 3);
+
+  let v = new Vector2(3, -2);
+  const vDefault = new Vector2(3, -2);
+
+  $: vPlusW = w.clone().add(v);
+  $: vMinusW = v.clone().sub(w);
+</script>
+
+<Canvas2D gridType={GridType.squareGrid}>
+  <Draggables snap bind:position={w} defaultPosition={wDefault} color={PrimeColor.ultramarine} />
+  <Draggables snap bind:position={v} defaultPosition={vDefault} color={PrimeColor.ultramarine} />
+
+  <!-- Arcs -->
+  <Arc2D points={[v, w]} distance={0.75} color={PrimeColor.green} />
+
+  <!-- Bases -->
+  <Vector2D direction={v} length={v.length()} color={PrimeColor.ultramarine}>
+    <Latex2D latex={'\\vec{v}'} offset={new Vector2(-0.2, 0.5)} />
+  </Vector2D>
+  <Vector2D direction={w} length={w.length()} color={PrimeColor.green}>
+    <Latex2D latex={'\\vec{w}'} offset={new Vector2(-0.2, 0.2)} />
+  </Vector2D>
+
+  <!-- v+w -->
+  <Vector2D direction={vPlusW} length={vPlusW.length()} color={PrimeColor.red}>
+    <Latex2D latex={'\\vec{v} + \\vec{w}'} offset={new Vector2(0.2, 0.2)} />
+  </Vector2D>
+
+  <!-- v-w -->
+  <Vector2D origin={w} direction={vMinusW} length={vMinusW.length()} color={PrimeColor.pink}>
+    <Latex2D latex={'\\vec{v} - \\vec{w}'} offset={new Vector2(-0.2, -0.2)} />
+  </Vector2D>
+
+  <!-- Helper lins -->
+  <Vector2D
+    origin={w}
+    direction={v}
+    length={v.length()}
+    color={PrimeColor.black}
+    isDashed
+    hideHead
+  />
+  <Vector2D
+    origin={v}
+    direction={w}
+    length={w.length()}
+    color={PrimeColor.black}
+    isDashed
+    hideHead
+  />
+</Canvas2D>
