ba1edc32f60244eb1a6ddad68711f6a460a48d99
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-09-14 10:17:02 +0200

    fix passing variables to canvas

--- src/lib/p5-components/components/Canvas.svelte
+++ src/lib/p5-components/components/Canvas.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
   import AbstractCanvas from '$lib/components/AbstractCanvas.svelte';
+  import type { Sliders } from '$lib/utils/Slider';
   import Axis2D from './Axis.svelte';
   import { GridType } from './Grids';
   import P5Canvas from './P5Canvas.svelte';
 
+  export let title = '';
+  export let background = '#ffffff';
+  export let showFormulasDefault = false;
+  export let isIframe = false; // Is the scene inside an iframe?
+  export let sliders: Sliders | undefined = undefined;
+
   export let gridType: GridType = GridType.none;
 </script>
 
-<AbstractCanvas let:width let:height>
+<AbstractCanvas
+  {title}
+  {background}
+  {showFormulasDefault}
+  {isIframe}
+  let:width
+  let:height
+  bind:sliders
+>
   <P5Canvas {gridType} size={{ width, height }}>
     <!-- Grids -->
     <slot name="grids">
--- src/lib/threlte-components/Canvas.svelte
+++ src/lib/threlte-components/Canvas.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
   import AbstractCanvas from '$lib/components/AbstractCanvas.svelte';
-  import { Canvas } from '@threlte/core';
-  import SetCamera from './SetCamera.svelte';
-  import { Vector3 } from 'three';
-  import { T } from '@threlte/core';
   import type { Sliders } from '$lib/utils/Slider';
+  import { Canvas, T } from '@threlte/core';
+  import { Vector3 } from 'three';
+  import SetCamera from './SetCamera.svelte';
 
   export let cameraPosition = new Vector3(10, 10, 10);
   export let enablePan = false;
   export let zoom = 29;
+  export let title = '';
+  export let background = '#ffffff';
+  export let showFormulasDefault = false;
+  export let isIframe = false; // Is the scene inside an iframe?
   export let sliders: Sliders | undefined = undefined;
 </script>
 
-<AbstractCanvas let:width let:height let:resetKey bind:sliders>
+<AbstractCanvas
+  {title}
+  {background}
+  {showFormulasDefault}
+  {isIframe}
+  let:width
+  let:height
+  let:resetKey
+  bind:sliders
+>
   <Canvas size={{ width, height }}>
     <SetCamera position={cameraPosition} {resetKey} {enablePan} {zoom} />
 
