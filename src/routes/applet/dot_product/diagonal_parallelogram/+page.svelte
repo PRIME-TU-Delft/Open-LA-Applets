df0c54fb0f9b23c79c6d5f32400d282a090c1a78
PHengst <paulinehengst1@gmail.com>
2023-09-15 11:13:50 +0200

    left hand side of img

--- src/routes/applet/dot_product/diagonal_parallelogram/+page.svelte
+++ src/routes/applet/dot_product/diagonal_parallelogram/+page.svelte
@@ -10,26 +10,26 @@
   //todo consistant var naming -> camelcase or __
 
   //vars for static section
-  let o_static = new Vector2(-4, 0)
+  let o_static = new Vector2(-3, 0)
 
   let w_static = new Vector2(1.5, 3);
-  let v_static = new Vector2(4, -1);
+  let v_static = new Vector2(2, -1);
 
   let vPlusW_static = v_static.clone().add(w_static);
   let vMinusW_static = v_static.clone().sub(w_static);
 
 
   //vars for interactive section
-  let origin = new Vector2(2, 0);
+  let origin = new Vector2(1.5, 0);
 
-  let w_plus_o = new Vector2(3, 4);
-  const wDefault = new Vector2(3, 4);
+  let w_plus_o = new Vector2(2,3);
+  const wDefault = new Vector2(2,3);
   //w is calculated this way because a draggable takes a global position, not a vector relative to origin
   //TODO rename var to make this more logical
   $: w = w_plus_o.clone().sub(origin);
 
-  let v_plus_o = new Vector2(4, -2.5);
-  const vDefault = new Vector2(4, -2.5);
+  let v_plus_o = new Vector2(3.5, -2);
+  const vDefault = new Vector2(3.5, -2);
   $: v = v_plus_o.clone().sub(origin);
 
   $: vPlusW = v.clone().add(w);
@@ -40,7 +40,7 @@
 
 </script>
 
-<Canvas2D gridType={GridType.squareGrid}>
+<Canvas2D >
   <Draggables snap bind:position={w_plus_o} defaultPosition={wDefault} color={PrimeColor.green} />
   <Draggables snap bind:position={v_plus_o} defaultPosition={vDefault} color={PrimeColor.ultramarine} />
 
@@ -95,45 +95,45 @@
 
   <!-- Non interactive part of image -->
 
-      <RightAngle origin={origin} vs={[v,w]}/>
+      <RightAngle origin={o_static} vs={[v_static,w_static]}/>
 
       <!-- Bases -->
-      <Vector2D origin={origin} direction={v} length={v.length()} color={PrimeColor.ultramarine} let:endPoint>
+      <Vector2D origin={o_static} direction={v_static} length={v_static.length()} color={PrimeColor.ultramarine} let:endPoint>
         <Latex2D position={endPoint} latex={'v'} offset={new Vector2(-0.2, 0.5)} color={PrimeColor.ultramarine} />
       </Vector2D>
-      <Vector2D origin={origin} direction={w} length={w.length()} color={PrimeColor.green} let:endPoint>
+      <Vector2D origin={o_static} direction={w_static} length={w_static.length()} color={PrimeColor.green} let:endPoint>
         <Latex2D position={endPoint} latex={'w'} offset={new Vector2(-0.2, 0.2)} color={PrimeColor.green}/>
       </Vector2D>
 
       <!-- v+w -->
-      <Vector2D origin={origin} direction={vPlusW} length={vPlusW.length()} color={PrimeColor.red} let:endPoint>
+      <Vector2D origin={o_static} direction={vPlusW_static} length={vPlusW_static.length()} color={PrimeColor.red} let:endPoint>
         <Latex2D position={endPoint} latex={'v + w'} offset={new Vector2(0.2, 0.2)} color={PrimeColor.red} />
       </Vector2D>
 
       <!-- v-w -->
       <Vector2D
-        origin={w.clone().add(origin)}
-        direction={vMinusW}
-        length={vMinusW.length()}
+        origin={w_static.clone().add(o_static)}
+        direction={vMinusW_static}
+        length={vMinusW_static.length()}
         color={PrimeColor.yellow}
         let:endPoint
       >
         <Latex2D position={endPoint} latex={'v - w'} offset={new Vector2(-0.2, -0.2)} color={PrimeColor.yellow}/>
       </Vector2D>
 
-      <!-- Helper lins -->
+      <!-- Helper lines -->
       <Vector2D
-        origin={w.clone().add(origin)}
-        direction={v}
-        length={v.length()}
+        origin={w_static.clone().add(o_static)}
+        direction={v_static}
+        length={v_static.length()}
         color={PrimeColor.black}
         isDashed
         hideHead
       />
       <Vector2D
-        origin={v.clone().add(origin)}
-        direction={w}
-        length={w.length()}
+        origin={v_static.clone().add(o_static)}
+        direction={w_static}
+        length={w_static.length()}
         color={PrimeColor.black}
         isDashed
         hideHead
