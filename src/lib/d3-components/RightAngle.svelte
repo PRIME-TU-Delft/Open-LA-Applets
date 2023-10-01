13e7575d73f53468e0ba891f09971a775a219538
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-10-01 11:38:45 +0200

    added Point & Right angle

---
+++ src/lib/d3-components/RightAngle.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Vector2 } from 'three';
+  import Line from './Line.svelte';
+
+  export let vs: [Vector2, Vector2]; // vectors to draw angle between
+  export let origin: Vector2 = new Vector2(0, 0); // common orgin of vectors
+  export let size = 0.3; // size of drawn angle
+  export let color = 'black'; // color of used lines
+
+  //resize vectors
+  $: u1 = vs[0].clone().multiplyScalar(size / vs[0].length());
+  $: u2 = vs[1].clone().multiplyScalar(size / vs[1].length());
+</script>
+
+<!-- draw two lines to represent right angle if perpendicular -->
+
+{#if Math.abs(u1.dot(u2)) <= 0.00000000001 && !u1.equals(u2)}
+  <Line {color} start={u1.clone().add(origin)} end={u1.clone().add(u2).add(origin)} />
+  <Line {color} start={u2.clone().add(origin)} end={u1.clone().add(u2).add(origin)} />
+{/if}
--- src/lib/d3-components/index.ts
+++ src/lib/d3-components/index.ts
@@ -3,6 +3,8 @@ export { default as Axis2D } from './Axis.svelte';
 export { default as Canvas2D } from './Canvas.svelte';
 export { default as Latex2D } from './Latex.svelte';
 export { default as Line2D } from './Line.svelte';
+export { default as Point2D } from './Point.svelte';
+export { default as RightAngle } from './RightAngle.svelte';
 export { default as Vector2D } from './Vector.svelte';
 
 export { default as Draggable2D } from './Draggable.svelte';
