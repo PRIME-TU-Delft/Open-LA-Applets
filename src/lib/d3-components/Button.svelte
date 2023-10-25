60ff7b42055a81eb33a244d9b344b6f2592b5d7e
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-10-25 18:45:57 +0200

    added csf and canvas story

---
+++ src/lib/d3-components/Button.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let rounded = false;
+</script>
+
+<button class="btn" class:rounded on:click>
+  <slot />
+</button>
---
+++ src/lib/d3-components/stories/Canvas.stories.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" context="module">
+  import type { Meta } from '@storybook/svelte';
+  import Canvas from '../Canvas.svelte';
+
+  export const meta = {
+    title: '2d components/Canvas',
+    component: Canvas,
+    tags: ['autodocs'],
+    argTypes: {
+      title: { type: 'string' },
+      background: { type: 'string' }
+    }
+  } satisfies Meta<Canvas>;
+</script>
+
+<script>
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+  <!--👇 'on:click' allows to forward event to addon-actions  -->
+  <Canvas {...args} />
+</Template>
+
+<!-- Dynamic snippet should be disabled for this story -->
+<Story name="Default" source>
+  <Canvas />
+</Story>
+
+<!-- Dynamic snippet should be disabled for this story -->
+<Story name="With title" source args={{ title: 'Hello this is a title' }} />
+
+<Story name="With background" source args={{ background: 'red' }} />
