9d0b4af6293066c0f6a51f53ff3b182226cb6eca
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-12-05 15:34:09 +0100

    Run prettier and linter

--- src/routes/applet/basisdim/standardbasis/+page.svelte
+++ src/routes/applet/basisdim/standardbasis/+page.svelte
@@ -1,38 +1,31 @@
 <script lang="ts">
-  import { Axis2D, Canvas2D, Line2D, Vector2D, Latex2D } from '$lib/d3-components';
-
-  import { Vector2, Vector3 } from 'three';
-  import { Axis3D, Canvas3D, Latex3D, Line3D, Vector3D } from '$lib/threlte-components';
-  import LatexUI from '$lib/components/Latex.svelte';
+  import { Canvas2D, Latex2D, Vector2D } from '$lib/d3-components';
+  import { Axis3D, Canvas3D, Latex3D, Vector3D } from '$lib/threlte-components';
   import { PrimeColor } from '$lib/utils/PrimeColors';
-
-  let zoom = 1;
+  import { Vector2, Vector3 } from 'three';
 
   let e1 = new Vector2(1, 0);
   let e2 = new Vector2(0, 1);
 
-  let e1Length = 1
-  let e2Length = 1
+  let e1Length = 1;
+  let e2Length = 1;
 
   let te1 = new Vector3(0, 0, 1);
   let te2 = new Vector3(1, 0, 0);
   let te3 = new Vector3(0, 1, 0);
-
 </script>
 
 <div class="wrapper">
-
   <div class="panel">
     <Canvas2D>
-
       <!-- e1 -->
       <Vector2D direction={e1} length={e1Length} color={PrimeColor.ultramarine} let:endPoint>
-        <Latex2D latex={'\\mathbf{e_1}'} offset={new Vector2(-0.3, 0.4)} position={endPoint}/>
+        <Latex2D latex={'\\mathbf{e_1}'} offset={new Vector2(-0.3, 0.4)} position={endPoint} />
       </Vector2D>
 
       <!-- e2 -->
       <Vector2D direction={e2} length={e2Length} color={PrimeColor.green} let:endPoint>
-        <Latex2D latex={'\\mathbf{e_2}'} offset={new Vector2(0.2, -0.1)} position={endPoint}/>
+        <Latex2D latex={'\\mathbf{e_2}'} offset={new Vector2(0.2, -0.1)} position={endPoint} />
       </Vector2D>
     </Canvas2D>
   </div>
