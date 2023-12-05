eca40bbb0cad03e391ebab8f5f1fb2b5cfa3b89a
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-12-05 15:33:08 +0100

    Added split canvas with both 2D and 3D canvasses + Docs

--- src/lib/d3-components/Canvas.svelte
+++ src/lib/d3-components/Canvas.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
   import AbstractCanvas from '$lib/components/AbstractCanvas.svelte';
+  import SetCamera from '$lib/threlte-components/SetCamera.svelte';
   import type { Sliders } from '$lib/utils/Slider';
-  import { Vector2 } from 'three';
-  import { Latex2D } from '.';
+  import { Canvas, T } from '@threlte/core';
+  import { Vector3 } from 'three';
   import D3Canvas from './D3Canvas.svelte';
   import { GridType } from './grids/GridTypes';
 
+  export let cameraPosition = new Vector3(10, 10, 10);
+  export let enablePan = false;
   export let title = '';
   export let background = '#ffffff';
   export let showFormulasDefault = false;
   export let width = '100%';
   export let height = 'auto';
   export let zoom = 1;
+  export let zoom3d = 29;
 
   // Is the scene inside an iframe?
   export let isIframe = false;
@@ -27,11 +31,12 @@
   {isIframe}
   let:width
   let:height
+  let:resetKey
   bind:sliders
   --height={height}
   --width={width}
 >
-  {@const totalWidth = $$slots.splitCanvas ? width / 2 : width}
+  {@const totalWidth = $$slots.splitCanvas || $$slots.splitCanvas3d ? width / 2 : width}
 
   <D3Canvas {zoom} width={totalWidth} {height} {gridType}>
     <slot />
@@ -41,11 +46,24 @@
     <div class="canvasDivider" />
 
     <D3Canvas {zoom} width={totalWidth} {height} {gridType}>
-      <!-- origin label-->
-      <Latex2D latex={'O'} offset={new Vector2(-0.15, -0.16)} />
-
       <slot name="splitCanvas" />
     </D3Canvas>
+  {/if}
+
+  {#if $$slots.splitCanvas3d}
+    <div class="canvasDivider" />
+
+    <div class="canvas3d relative overflow-hidden">
+      <Canvas size={{ width: totalWidth, height }}>
+        <SetCamera position={cameraPosition} {resetKey} {enablePan} zoom={zoom3d} />
+
+        <slot name="lights">
+          <T.AmbientLight intensity={1} />
+        </slot>
+
+        <slot name="splitCanvas3d" />
+      </Canvas>
+    </div>
   {/if}
 
   <svelte:fragment slot="formulas"><slot name="formulas" /></svelte:fragment>
