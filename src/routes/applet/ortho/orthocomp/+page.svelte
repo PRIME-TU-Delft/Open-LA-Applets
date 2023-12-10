e5adb3e8b6ff81fc748ac4a206c7d7327d41699b
Jarno Berger <j.berger@student.tudelft.nl>
2023-12-10 22:57:13 +0100

    Created applet orthocomp
    Only need to remove the axis for the 2d part, the rest should be finished

--- src/routes/applet/ortho/orthocomp/+page.svelte
+++ src/routes/applet/ortho/orthocomp/+page.svelte
@@ -1,77 +1,68 @@
 <script lang="ts">
-  import { RightAngle, Axis2D, Canvas2D, Line2D, Vector2D, Latex2D } from '$lib/d3-components';
+  import { RightAngle, Canvas2D, Vector2D, Latex2D } from '$lib/d3-components';
 
   import { Vector2, Vector3 } from 'three';
-  import { Axis3D, Canvas3D, Latex3D, Line3D, Vector3D } from '$lib/threlte-components';
+  import { Axis3D, Canvas3D, Latex3D, Vector3D, PlaneFromNormal } from '$lib/threlte-components';
   import { PrimeColor } from '$lib/utils/PrimeColors';
 
   let v = new Vector2(3, -3);
   let v_ortho = new Vector2(3, 3);
 
-  let ve = new Vector3(3, -3, -3);
+  let ve = new Vector3(0, 0, 0);
+  let ve_ortho = new Vector3(0, 7, 0);
+
+  const n0 = new Vector3(0, 1, 0);
 </script>
 
-<div class="wrapper">
-  <div class="panel">
-    <Canvas2D>
-      <!-- v -->
-      <Vector2D
-        direction={v}
-        length={v.length() * 2}
-        color={PrimeColor.green}
-        origin={v.clone().multiplyScalar(-1)}
-      >
-        <Latex2D latex={'\\mathbf{v}'} position={v.clone().add(new Vector2(-0.3, -0.1))} />
-      </Vector2D>
+<Canvas2D
+  cameraPosition={new Vector3(10, 10, 13)}
+  zoom={1.5}
+  zoom3d={50}
+  title="The orthogonal complement of a line (left) and a plane (right)"
+>
+  <!-- v -->
+  <Vector2D
+    direction={v}
+    length={v.length() * 2}
+    color={PrimeColor.green}
+    origin={v.clone().multiplyScalar(-1)}
+  >
+    <Latex2D
+      latex={'\\mathbf{v}'}
+      position={v.clone().add(new Vector2(-0.3, -0.1))}
+      color={PrimeColor.green}
+    />
+  </Vector2D>
 
-      <!-- v_ortho -->
-      <Vector2D
-        direction={v_ortho}
-        length={v_ortho.length() * 2}
-        color={PrimeColor.ultramarine}
-        origin={v_ortho.clone().multiplyScalar(-1)}
-      >
-        <Latex2D
-          latex={'\\mathbf{v\\perp}'}
-          position={v_ortho.clone().add(new Vector2(-0.3, 0.3))}
-        />
-      </Vector2D>
+  <!-- v_ortho -->
+  <Vector2D
+    direction={v_ortho}
+    length={v_ortho.length() * 2}
+    color={PrimeColor.ultramarine}
+    origin={v_ortho.clone().multiplyScalar(-1)}
+  >
+    <Latex2D
+      latex={'\\mathbf{v\\perp}'}
+      position={v_ortho.clone().add(new Vector2(-0.3, 0.3))}
+      color={PrimeColor.ultramarine}
+    />
+  </Vector2D>
 
-      <!-- Arcs -->
-      <RightAngle vs={[v, v_ortho]} />
-    </Canvas2D>
-  </div>
+  <!-- Arcs -->
+  <RightAngle vs={[v, v_ortho]} lineWidth={0.03} />
 
-  <div class="panel">
-    <Canvas3D>
-      <!-- v -->
-      <Vector3D direction={ve} length={ve.length()} color={PrimeColor.green} />
+  <svelte:fragment slot="splitCanvas3d">
+    <!-- v -->
+    <PlaneFromNormal position={ve} normal={n0} color={PrimeColor.green} />
+    <Latex3D latex={'\\mathbf{v}'} position={new Vector3(5, 0, 5)} color={PrimeColor.green} />
 
-      <Latex3D latex={'T(\\mathbf{v})'} position={ve} />
-
-      <Axis3D />
-    </Canvas3D>
-  </div>
-</div>
-
-<style>
-  .formula {
-    position: absolute;
-    left: 50%;
-    top: 30%;
-    scale: 1.5;
-    transform: translate(-50%, -50%);
-    z-index: 100;
-  }
-
-  .wrapper {
-    display: flex;
-    gap: 1rem;
-    height: 100%;
-  }
-
-  .panel {
-    width: 50vw;
-    height: 100%;
-  }
-</style>
+    <!-- v_ortho -->
+    <Vector3D
+      direction={ve_ortho}
+      length={ve_ortho.length() * 2}
+      color={PrimeColor.ultramarine}
+      origin={ve_ortho.clone().multiplyScalar(-1)}
+    />
+    <Latex3D latex={'\\mathbf{v\\perp}'} position={ve_ortho} color={PrimeColor.ultramarine} />
+  </svelte:fragment>
+</Canvas2D>
