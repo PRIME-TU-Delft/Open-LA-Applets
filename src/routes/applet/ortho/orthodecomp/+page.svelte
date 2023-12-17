ec6261ec96684bea60b1be85948d6ed57452034f
Jarno Berger <j.berger@student.tudelft.nl>
2023-12-17 16:00:13 +0100

    Run prettier
    Forgot to ran prettier

--- src/routes/applet/ortho/orthodecomp/+page.svelte
+++ src/routes/applet/ortho/orthodecomp/+page.svelte
@@ -2,9 +2,16 @@
   import { RightAngle, Canvas2D, Vector2D, Latex2D } from '$lib/d3-components';
 
   import { Vector2, Vector3 } from 'three';
-  import { Axis3D, Canvas3D, Latex3D, Vector3D, PlaneFromNormal, Point3D } from '$lib/threlte-components';
+  import {
+    Axis3D,
+    Canvas3D,
+    Latex3D,
+    Vector3D,
+    PlaneFromNormal,
+    Point3D
+  } from '$lib/threlte-components';
   import { PrimeColor } from '$lib/utils/PrimeColors';
-    import Vector from '$lib/d3-components/Vector.svelte';
+  import Vector from '$lib/d3-components/Vector.svelte';
 
   let V = new Vector3(0, 0, 0);
   const n0 = new Vector3(0, 1, 0);
@@ -16,11 +23,7 @@
   let v = new Vector3(3, 4, 1.5);
 </script>
 
-<Canvas3D
-cameraPosition={new Vector3(10, 10, 17)}
-zoom={50}
-title="Name to be inserted"
->
+<Canvas3D cameraPosition={new Vector3(10, 10, 17)} zoom={50} title="Name to be inserted">
   <!-- v -->
   <Vector3D direction={v} length={v.length()} color={PrimeColor.ultramarine} />
   <Latex3D latex={'\\mathbf{v}'} position={v} color={PrimeColor.ultramarine} />
@@ -31,7 +34,11 @@ title="Name to be inserted"
 
   <!-- proj_v -->
   <Vector3D direction={proj_v} length={proj_v.length()} color={PrimeColor.red} />
-  <Latex3D latex={'Proj_V(\\mathbf{v})'} position={proj_v.clone().add(new Vector3(0.2, 0, 0.2))} color={PrimeColor.red} />
+  <Latex3D
+    latex={'Proj_V(\\mathbf{v})'}
+    position={proj_v.clone().add(new Vector3(0.2, 0, 0.2))}
+    color={PrimeColor.red}
+  />
 
   <!-- Dotted lines v and ProjV(v) -->
   <Vector3D
@@ -54,5 +61,4 @@ title="Name to be inserted"
   <!-- V -->
   <PlaneFromNormal position={V} normal={n0} color={PrimeColor.green} />
   <Latex3D latex={'V'} position={new Vector3(5.3, 0, 5.4)} color={PrimeColor.green} />
-
 </Canvas3D>
