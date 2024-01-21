4a9e756d48ce75acb703dc047da97599220ee3db
Jarno Berger <j.berger@student.tudelft.nl>
2024-01-21 16:10:36 +0100

    Added vector to show linear combinations
    New vector " x1u + x2v" was added where x1 and x2 are based on the sliders position

--- src/routes/applet/linear_combinations/span_two_line/+page.svelte
+++ src/routes/applet/linear_combinations/span_two_line/+page.svelte
@@ -1,29 +1,43 @@
 <script>
   import { Vector3 } from 'three';
   import { Axis3D, Canvas3D, Latex3D, Line3D, Vector3D } from '$lib/threlte-components';
+  import { Formula } from '$lib/utils/Formulas';
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Sliders } from '$lib/utils/Slider';
 
-  $: v = new Vector3(3, 2, -3).normalize().multiplyScalar(3); // Vector v;
-  $: u = v.clone().multiplyScalar(2); // Vector u = 2 * v;
-  $: lineDir = v.clone().normalize().multiplyScalar(10);
+  const v = new Vector3(3, 2, -3).normalize().multiplyScalar(3); // Vector v;
+  const u = v.clone().multiplyScalar(2); // Vector u = 2 * v;
+  const lineDir = v.clone().multiplyScalar(6);
+  let sliders = new Sliders().addSlider(1, -2, 2, 0.2).addSlider(1, -2, 2, 0.2);
 
-  let sliders = new Sliders().addSlider(3, 3, 5.5, 0.3).addSlider(6, 6, 8.5, 0.3);
+  $: x1u_plus_x2v = v.clone().multiplyScalar(sliders.x).add(u.clone().multiplyScalar(sliders.y))
+  $: formulax1 = new Formula('x1 = \\$', sliders.y, PrimeColor.darkGreen);
+  $: formulax2 = new Formula('x2 = \\$', sliders.x, PrimeColor.darkGreen);
 </script>
 
-<Canvas3D bind:sliders>
-  <Vector3D direction={v} length={sliders.x} striped radius={0.08} color={PrimeColor.red} />
+<Canvas3D 
+  formulas={[formulax1, formulax2]}
+  bind:sliders
+>
+  <Vector3D direction={v} length={v.length()} striped radius={0.08} color={PrimeColor.red} />
   <Latex3D
     latex={'\\mathbf{v}'}
-    position={v.clone().normalize().multiplyScalar(sliders.x).add(new Vector3(0, 0.5, 0))}
+    position={v.clone().add(new Vector3(0, 0.5, 0))}
     color={PrimeColor.red}
   />
 
-  <Vector3D direction={u} length={sliders.y} color={PrimeColor.yellow} />
+  <Vector3D direction={u} length={u.length()} color={PrimeColor.yellow} />
   <Latex3D
     latex={'\\mathbf{u}'}
-    position={u.clone().normalize().multiplyScalar(sliders.y).add(new Vector3(0, 0.5, 0))}
+    position={u.clone().add(new Vector3(0, 0.5, 0))}
     color={PrimeColor.yellow}
+  />
+
+  <Vector3D direction={x1u_plus_x2v} length={x1u_plus_x2v.length()} color={PrimeColor.orange} />
+  <Latex3D
+    latex={'\\mathbf{x_1u + x_2v}'}
+    position={x1u_plus_x2v.clone().add(new Vector3(0, 0.5, 0))}
+    color={PrimeColor.orange}
   />
 
   <Line3D points={[lineDir.clone().multiplyScalar(-1), lineDir]} color={PrimeColor.blue} />
--- src/routes/applet/linear_combinations/span_two_plane/+page.svelte
+++ src/routes/applet/linear_combinations/span_two_plane/+page.svelte
@@ -9,28 +9,39 @@
 
   const labelPosition = u.clone().add(v).normalize().multiplyScalar(8); // only used for label -> Span{u,v}
 
-  let sliders = new Sliders().addSlider(3, 3, 6, 0.3).addSlider(3, 3, 6, 0.3);
+  let sliders = new Sliders().addSlider(1, -2, 2, 0.2).addSlider(1, -2, 2, 0.2);
+
+  //Slider y is red so it is the scalar of the red vector u, the same goes for yellow with slider x and vector v
+  $: x1u_plus_x2v = u.clone().multiplyScalar(sliders.y).add(v.clone().multiplyScalar(sliders.x));
 </script>
 
 <Canvas3D bind:sliders>
   <!-- Vector u -->
-  <Vector3D direction={u} length={sliders.y} color={PrimeColor.yellow} />
+  <Vector3D direction={u} length={u.length()} color={PrimeColor.yellow} />
   <Latex3D
     latex={'\\mathbf{u}'}
-    position={u.clone().normalize().multiplyScalar(sliders.y)}
+    position={u}
     color={PrimeColor.yellow}
   />
 
   <!-- Vector v -->
-  <Vector3D direction={v} length={sliders.x} color={PrimeColor.red} />
+  <Vector3D direction={v} length={v.length()} color={PrimeColor.red} />
   <Latex3D
     latex={'\\mathbf{v}'}
-    position={v.clone().normalize().multiplyScalar(sliders.x)}
+    position={v}
     color={PrimeColor.red}
   />
 
+  <!-- Vector x1u + x2v -->
+  <Vector3D direction={x1u_plus_x2v} length={x1u_plus_x2v.length()} color={PrimeColor.orange} />
+  <Latex3D
+    latex={'\\mathbf{x_1u + x_2v}'}
+    position={x1u_plus_x2v}
+    color={PrimeColor.orange}
+  />
+
   <!-- Plane span -->
-  <PlaneFromPoints points={[new Vector3(0, 0, 0), u, v]} color={PrimeColor.blue} size={15} />
+  <PlaneFromPoints points={[new Vector3(0, 0, 0), u, v]} color={PrimeColor.blue} size={21} />
   <Latex3D
     latex={'Span\\{\\mathbf{u},\\mathbf{v}\\}'}
     position={labelPosition}
