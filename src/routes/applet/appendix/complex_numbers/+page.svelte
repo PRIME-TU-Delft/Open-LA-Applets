ab4b091bcbc778b58da0395342c4f9bb1d6efea7
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-02-09 13:42:21 +0100

    new sldiers of m,a,b

--- src/routes/applet/appendix/complex_numbers/+page.svelte
+++ src/routes/applet/appendix/complex_numbers/+page.svelte
@@ -9,82 +9,99 @@
   import { Vector2 } from 'three';
 
   let sliders = new Sliders()
-    .add(new Slider(3, 3, 10, 1, PrimeColor.blue))
-    .add(new Slider(0.25, 0, 1, 0.125, PrimeColor.darkGreen));
+    .add(new Slider(3, 3, 8, 1, PrimeColor.blue)) // m
+    .add(new Slider(16, 8, 64, 8, PrimeColor.darkGreen)) // a
+    .add(new Slider(16, 8, 64, 8, PrimeColor.red)); // b
 
-  function createFormulas(fraction: number, offset: number) {
-    return [
-      new Formula(
-        '\\text{offset} = \\frac{\\$}{4} \\pi',
-        Math.round(offset * 4).toFixed(1),
-        PrimeColor.darkGreen
-      ),
-      new Formula(
-        '\\text{arc length} = \\frac{2}{\\$} \\pi',
-        Math.round(fraction).toFixed(1),
-        PrimeColor.red
-      )
-    ];
+  function cartesian2Polar(vec2: Vector2, offset: Vector2 = new Vector2(0, 0)) {
+    const radius = offset.distanceTo(vec2);
+    const angleInRadians = Math.atan2(vec2.y, vec2.x); //This takes y first
+    return { radius, angleInRadians };
   }
-
-  // const formulas = [new Formula('\\text{offset} = \\$ \\pi', sliders.y, PrimeColor.darkGreen)];
-  $: formulas = createFormulas(sliders.x, sliders.y);
-
-  const distance = 2 * Math.sqrt(2);
 
   function polarToCartesian(
     radius: number,
     angleInRadians: number,
-    centerX: number = 0,
-    centerY: number = 0
+    offset: Vector2 = new Vector2(0, 0)
   ) {
     return new Vector2(
-      centerX + radius * Math.cos(angleInRadians),
-      centerY + radius * Math.sin(angleInRadians)
+      offset.x + radius * Math.cos(angleInRadians),
+      offset.y + radius * Math.sin(angleInRadians)
     );
   }
+
+  $: polarForm = cartesian2Polar(new Vector2(sliders.y, sliders.z));
+  $: radius = Math.pow(polarForm.radius, 1 / sliders.x);
+  $: angle = polarForm.angleInRadians;
+
+  function createFormulas(m: number, a: number, b: number) {
+    const piAngle = polarForm.angleInRadians / Math.PI;
+
+    return [
+      new Formula(`z^{\\$1} = -\\$2 + \\$3i`)
+        .addAutoParam(m, PrimeColor.blue)
+        .addAutoParam(a, PrimeColor.darkGreen)
+        .addAutoParam(b, PrimeColor.red),
+      new Formula(
+        `r = \\sqrt[\\$]{${polarForm.radius.toFixed(2)}} = ${radius.toFixed(2)}`,
+        m,
+        PrimeColor.blue
+      ),
+      new Formula(
+        `\\phi = \\frac{\\arg(${a.toFixed(2)} + ${b.toFixed(2)}i)}{\\$} = \\frac{${(
+          piAngle * m
+        ).toFixed(2)}}{\\$}\\pi = ${piAngle.toFixed(2)}\\pi`,
+        m,
+        PrimeColor.blue
+      )
+    ];
+  }
+
+  $: formulas = createFormulas(sliders.x, sliders.y, sliders.z);
 </script>
 
 <Canvas2D showAxisNumbers={false} bind:sliders {formulas}>
   <!-- Start angle (green) -->
-  <Point position={polarToCartesian(distance, Math.PI * sliders.y)} color={PrimeColor.blue} />
+  <Point position={polarToCartesian(radius, angle)} color={PrimeColor.blue} />
   <Line2D
-    start={polarToCartesian(distance, Math.PI * sliders.y)}
-    end={polarToCartesian(
-      distance,
-      ((Math.PI * 2) / sliders.x) * (sliders.x - 1) + Math.PI * sliders.y
-    )}
+    start={polarToCartesian(radius, angle)}
+    end={polarToCartesian(radius, angle + (Math.PI * 2) / sliders.x)}
     color={PrimeColor.blue}
   />
 
-  <Angle
-    color={PrimeColor.darkGreen}
-    hasHead
-    {distance}
-    startAngle={0}
-    endAngle={Math.PI * sliders.y}
-  />
+  <Angle color={PrimeColor.darkGreen} hasHead distance={radius} startAngle={0} endAngle={angle} />
 
   <!-- Intermediate angles (red) -->
   {#each new Array(sliders.x) as _, i}
-    {@const startAngle = ((Math.PI * 2) / sliders.x) * i + Math.PI * sliders.y}
+    {@const startAngle = angle + (Math.PI * 2 * i) / sliders.x}
     {@const endAngle = startAngle + (Math.PI * 2) / sliders.x}
-    {@const startPos = polarToCartesian(distance, startAngle)}
-    {@const endPos = polarToCartesian(distance, endAngle)}
+    {@const startPos = polarToCartesian(radius, startAngle)}
+    {@const endPos = polarToCartesian(radius, endAngle)}
+    {@const latexPos = polarToCartesian(radius + 0.5, endAngle)}
 
     <Line2D start={startPos} end={endPos} color={PrimeColor.blue} />
 
-    {@const latexPos = polarToCartesian(distance + 0.5, endAngle)}
-    <Latex2D latex={endPos.x.toFixed(2) + ' + ' + endPos.y.toFixed(2) + 'i'} position={latexPos} />
+    {#key endAngle}
+      <Latex2D
+        latex={endPos.x.toFixed(2) + ' + ' + endPos.y.toFixed(2) + 'i'}
+        position={latexPos}
+      />
+    {/key}
 
     {#if startAngle > Math.PI * 2}
       <Point position={endPos} color={PrimeColor.blue} />
     {:else if endAngle < Math.PI * 2}
       <Point position={endPos} color={PrimeColor.blue} />
-      <Angle color={PrimeColor.red} {distance} {startAngle} {endAngle} hasHead />
+      <Angle color={PrimeColor.red} distance={radius} {startAngle} {endAngle} hasHead />
     {:else}
       <Point position={endPos} color={PrimeColor.blue} />
-      <Angle color={PrimeColor.yellow} {distance} {startAngle} endAngle={Math.PI * 2} hasHead />
+      <Angle
+        color={PrimeColor.yellow}
+        distance={radius}
+        {startAngle}
+        endAngle={Math.PI * 2}
+        hasHead
+      />
     {/if}
   {/each}
 </Canvas2D>
