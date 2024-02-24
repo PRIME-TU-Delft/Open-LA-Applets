8340003b25e3270be609070a1a225525519271a0
Jarno Berger <j.berger@student.tudelft.nl>
2024-02-24 13:05:02 +0100

    color/latex names fixes

--- src/routes/applet/lines_and_planes/normal_equation_line/+page.svelte
+++ src/routes/applet/lines_and_planes/normal_equation_line/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-  import { RightAngle, Canvas2D, Latex2D, Vector2D, Draggable2D, Line2D, Point2D } from '$lib/d3-components';
+  import {
+    RightAngle,
+    Canvas2D,
+    Latex2D,
+    Vector2D,
+    Draggable2D,
+    Line2D,
+    Point2D
+  } from '$lib/d3-components';
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Vector2 } from 'three';
   import { Formula } from '$lib/utils/Formulas';
@@ -11,19 +19,27 @@
   $: PQ = Q.clone().sub(P);
   $: N = new Vector2(1, 2);
 
-  $: L_dir = new Vector2(N.y, N.clone().x * -1); 
+  $: L_dir = new Vector2(N.y, N.clone().x * -1);
   $: L_start = L_dir.clone().multiplyScalar(-10).add(P);
   $: L_end = L_dir.clone().multiplyScalar(10).add(P);
   $: L_label = L_dir.clone().multiplyScalar(4).add(new Vector2(0, 2));
 
   function round(x: number) {
-    return Math.round(x * 100) / 100
+    return Math.round(x * 100) / 100;
   }
 
   function setFormulas(n: Vector2, pq: Vector2) {
-    const f1 = new Formula('\\vec{n} = \\begin{bmatrix} ' + round(n.x) + ' \\\\ ' + round(n.y) + ' \\end{bmatrix}');
-    const f2 = new Formula('\\vec{PQ} = \\begin{bmatrix} ' + round(pq.x) + ' \\\\ ' + round(pq.y) + ' \\end{bmatrix}');
-    const f3 = new Formula('\\vec{n} \\cdot \\vec{PQ} = ' + round(n.dot(pq)))
+    const f1 = new Formula(
+      '\\mathbf{n} = \\begin{bmatrix} ' + round(n.x) + ' \\\\ ' + round(n.y) + ' \\end{bmatrix}'
+    );
+    const f2 = new Formula(
+      '\\overrightarrow{PQ} = \\begin{bmatrix} ' +
+        round(pq.x) +
+        ' \\\\ ' +
+        round(pq.y) +
+        ' \\end{bmatrix}'
+    );
+    const f3 = new Formula('\\mathbf  {n} \\cdot \\overrightarrow{PQ} = ' + round(n.dot(pq)));
 
     formulas = [f1, f2, f3];
   }
@@ -36,44 +52,48 @@
     const distance = Math.abs(a * point.x + b * point.y + c) / Math.sqrt(a * a + b * b);
 
     //Calculate the closest point from the line to point q
-    const x3 = ((b * b * point.x) - (a * b * point.y) - (a * c)) / (a * a + b * b);
-    const y3 = ((a * a * point.y) - (a * b * point.x) - (b * c)) / (a * a + b * b);
+    const x3 = (b * b * point.x - a * b * point.y - a * c) / (a * a + b * b);
+    const y3 = (a * a * point.y - a * b * point.x - b * c) / (a * a + b * b);
 
     if (distance < 0.2) {
       Q = new Vector2(x3, y3);
       PQ = Q.clone().sub(P);
-      setFormulas(N, PQ);
     }
+    setFormulas(N, PQ);
   }
 
-  $: setFormulas(N, PQ);
   $: snapQ(Q);
-
 </script>
 
-<Canvas2D zoom={0.9} {formulas} >
-  
+<Canvas2D zoom={0.9} {formulas}>
   <Draggable2D id="normal_equation_line1" bind:position={Q} color={PrimeColor.red} />
 
   <!-- RightAngle -->
-  <RightAngle vs={[N, PQ]} size={0.3} lineWidth={0.02} origin={P}/>
+  <RightAngle vs={[N, PQ]} size={0.3} lineWidth={0.02} origin={P} />
 
   <!-- Line L-->
-  <Line2D start={L_start} end={L_end} color={PrimeColor.blue}></Line2D>
+  <Line2D start={L_start} end={L_end} color={PrimeColor.blue} />
   <Latex2D latex={'\\mathcal{L}'} position={L_label} color={PrimeColor.blue} />
 
   <!-- Vectors -->
-  <Vector2D origin={P} direction={PQ} length={PQ.length()} color={PrimeColor.darkGreen}></Vector2D>
+  <Vector2D origin={P} direction={PQ} length={PQ.length()} color={PrimeColor.darkGreen} />
 
-  <Vector2D origin={P} direction={N} length={N.length()} color={PrimeColor.blue}>
-    <Latex2D latex={'\\vec{n}'} position={N.clone().add(P).add(new Vector2(0.1, 0.25))} color={PrimeColor.blue} />
+  <Vector2D origin={P} direction={N} length={N.length()} color={PrimeColor.pink}>
+    <Latex2D
+      latex={'\\mathbf{n}'}
+      position={N.clone().add(P).add(new Vector2(0.1, 0.25))}
+      color={PrimeColor.pink}
+    />
   </Vector2D>
 
   <!-- Points -->
-  <Point2D position={P} color={PrimeColor.yellow}></Point2D>
-  <Latex2D latex={'P'} position={P.clone().add(new Vector2(-0.1, -0.2))} color={PrimeColor.yellow} />
+  <Point2D position={P} color={PrimeColor.yellow} />
+  <Latex2D
+    latex={'P'}
+    position={P.clone().add(new Vector2(-0.1, -0.2))}
+    color={PrimeColor.yellow}
+  />
 
-  <Point2D position={Q} color={PrimeColor.red}></Point2D>
+  <Point2D position={Q} color={PrimeColor.red} />
   <Latex2D latex={'Q'} position={Q.clone().add(new Vector2(-0.1, -0.2))} color={PrimeColor.red} />
-
 </Canvas2D>
