1b37ba95fffecb3a05642de8f8f548944c5bb068
PHengst <paulinehengst1@gmail.com>
2024-03-13 17:56:04 +0100

    spans & formulas are correct

--- src/routes/applet/linear_combinations/span_three_plane/+page.svelte
+++ src/routes/applet/linear_combinations/span_three_plane/+page.svelte
@@ -3,7 +3,7 @@
   import { Axis3D, Canvas3D, Latex3D, PlaneFromPoints, Vector3D } from '$lib/threlte-components';
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Controls } from '$lib/utils/Controls';
-    import { Formula } from '$lib/utils/Formulas';
+  import { Formula } from '$lib/utils/Formulas';
 
   const u = new Vector3(1, 2, -2); // Vector U;
   const v = new Vector3(3, 1, 0); // Vector V;
@@ -12,7 +12,6 @@
 
   let controls = Controls.addToggle(true, '\\mathbf{u}')
     .addToggle(true, '\\mathbf{v}')
-    .addToggle(true, '\\mathbf{w}');
 
   /**
    * Creates formulas based on the given inputs.
@@ -21,7 +20,8 @@
    * @param {number} b - The second input number.
    * @returns {string[]} - An array of formulas.
    */
-  function createFormulas(t1: boolean, t2: boolean, t3: boolean) {
+  function createFormulas(t1: boolean, t2: boolean) {
+    const t3 = true;
     const formulaString = [t1, t2, t3]
       .filter((t) => t)
       .map((_, i) => {
@@ -31,15 +31,18 @@
 
     let formula = new Formula('\\mathbf{Span}\\{' + formulaString + '\\}');
 
+    
+
     // Selectivly add parameters to the formula
     if (t1) formula = formula.addAutoParam('u', PrimeColor.blue);
-    if (t2) formula = formula.addAutoParam('v', PrimeColor.darkGreen);
-    if (t3) formula = formula.addAutoParam('w', PrimeColor.red);
+    if (t2) formula = formula.addAutoParam('v', PrimeColor.red);
+
+    formula = formula.addAutoParam('w', PrimeColor.darkGreen);
 
     return [formula];
   }
 
-  $: formulas = createFormulas(controls[0], controls[1], controls[2]);
+  $: formulas = createFormulas(controls[0], controls[1]);
 
 </script>
 
@@ -49,7 +52,7 @@
   <!-- Vector u -->
   <Vector3D direction={u} length={u.length()} color={PrimeColor.blue} />
   <Latex3D latex={'\\mathbf{u}'} position={u} color={PrimeColor.blue} />
-  {#if controls[2]}
+
     <Vector3D
     origin={u}
     direction={v}
@@ -58,7 +61,7 @@
     hideHead
     color={PrimeColor.black}
   />
-  {/if}
+
   
 {/if}
 
@@ -66,7 +69,7 @@
   <!-- Vector v -->
   <Vector3D direction={v} length={v.length()} color={PrimeColor.red} />
   <Latex3D latex={'\\mathbf{v}'} position={v} color={PrimeColor.red} />
-  {#if controls[2]}
+
     <!-- helper striped Vectors -->
     <Vector3D
     origin={v}
@@ -76,14 +79,22 @@
     hideHead
     color={PrimeColor.black}
   />
-  {/if}
+
 {/if}
-{#if controls[2]}
+
+{#if !controls[0] && !controls[1]}
+  <!-- Line span -->
+<Vector3D origin={w.clone().normalize().multiplyScalar(-15)} direction={w} length={30} color={PrimeColor.yellow} hideHead radius={0.04} />
+{:else}
+  <!-- Plane span -->
+  <PlaneFromPoints points={[new Vector3(0, 0, 0), u, v]} color={PrimeColor.yellow} size={15} />
+{/if}
+
+
   <!-- Vector w -->
   <Vector3D direction={w} length={w.length()} color={PrimeColor.darkGreen} />
   <Latex3D latex={'\\mathbf{w}'} position={w} color={PrimeColor.darkGreen} />
   
-{/if}
 
 
 
@@ -91,13 +102,6 @@
 
 
 
-
-
-
-
-
-  <!-- Plane span -->
-  <PlaneFromPoints points={[new Vector3(0, 0, 0), u, v]} color={PrimeColor.yellow} size={15} />
   <Latex3D
     latex={'\\mathrm{Span}\\{\\mathbf{u},\\mathbf{v},\\mathbf{w}\\}'}
     position={w.clone().normalize().multiplyScalar(8)}
