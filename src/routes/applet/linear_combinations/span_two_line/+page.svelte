5cd015e985bf15ed22b792397f31894123d487f1
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-03-17 13:54:01 +0100

    Add controls for sliders in span_two_line page

--- src/routes/applet/linear_combinations/span_two_line/+page.svelte
+++ src/routes/applet/linear_combinations/span_two_line/+page.svelte
@@ -2,24 +2,35 @@
   import { Vector3 } from 'three';
   import { Axis3D, Canvas3D, Latex3D, Line3D, Vector3D } from '$lib/threlte-components';
   import { PrimeColor } from '$lib/utils/PrimeColors';
+  import { Controls } from '$lib/utils/Controls';
+
+  let controls = Controls.addSlider(2, -5, 5, 0.5, PrimeColor.red).addSlider(
+    5,
+    -5,
+    5,
+    0.5,
+    PrimeColor.yellow
+  );
 
   $: v = new Vector3(3, 2, -3).normalize().multiplyScalar(3); // Vector v;
   $: u = v.clone().multiplyScalar(2); // Vector u = 2 * v;
   $: lineDir = v.clone().normalize().multiplyScalar(10);
 </script>
 
-<Canvas3D>
-  <Vector3D direction={v} length={v.length()} striped radius={0.08} color={PrimeColor.red} />
+<Canvas3D bind:controls>
+  <Vector3D direction={v} length={controls[0]} striped radius={0.08} color={PrimeColor.red} />
   <Latex3D
     latex={'\\mathbf{v}'}
-    position={v.clone().add(new Vector3(0, 0.5, 0))}
+    position={v.clone().normalize().multiplyScalar(controls[0])}
+    offset={0.5}
     color={PrimeColor.red}
   />
 
-  <Vector3D direction={u} length={u.length()} color={PrimeColor.yellow} />
+  <Vector3D direction={u} length={controls[1]} color={PrimeColor.yellow} />
   <Latex3D
     latex={'\\mathbf{u}'}
-    position={u.clone().add(new Vector3(0, 0.5, 0))}
+    position={u.clone().normalize().multiplyScalar(controls[1])}
+    offset={0.8}
     color={PrimeColor.yellow}
   />
 
