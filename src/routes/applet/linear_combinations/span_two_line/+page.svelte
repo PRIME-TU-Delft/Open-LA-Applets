c5b989f4840b38efe7f2d88a381f54cfcf7aa20b
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-03-22 15:02:37 +0100

    fixed wrong color

--- src/routes/applet/linear_combinations/span_two_line/+page.svelte
+++ src/routes/applet/linear_combinations/span_two_line/+page.svelte
@@ -4,15 +4,9 @@
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Controls } from '$lib/utils/Controls';
 
-  let controls = Controls.addSlider(
-    2,
-    -5,
-    5,
-    Math.PI / 6,
-    PrimeColor.raspberry,
-    'length of v',
-    (n) => n.toFixed(1)
-  ).addSlider(5, -5, 5, Math.PI / 6, PrimeColor.yellow, 'length of u', (n) => n.toFixed(1));
+  let controls = Controls.addSlider(2, -5, 5, Math.PI, PrimeColor.raspberry, 'length of v', (n) =>
+    n.toFixed(1)
+  ).addSlider(5, -5, 5, Math.PI, PrimeColor.yellow, 'length of u', (n) => n.toFixed(1));
 
   $: v = new Vector3(3, 2, -3).normalize().multiplyScalar(3); // Vector v;
   $: u = v.clone().multiplyScalar(2); // Vector u = 2 * v;
@@ -22,7 +16,7 @@
 <Canvas3D bind:controls>
   <!-- If u & v are on the same quadrant and v is SMALLER OR EQUAL TO than u -> u on top -->
   {#if controls[0] * controls[1] > 0 && Math.abs(controls[0]) <= Math.abs(controls[1])}
-    <Vector3D direction={v} length={controls[0]} color={PrimeColor.darkGreen} alwaysOnTop />
+    <Vector3D direction={v} length={controls[0]} color={PrimeColor.raspberry} alwaysOnTop />
   {:else}
     <Vector3D direction={v} length={controls[0]} color={PrimeColor.raspberry} />
   {/if}
