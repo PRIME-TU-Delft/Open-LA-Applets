6a19b03824520f4882094b9367692df2fba5b4a6
PHengst <paulinehengst1@gmail.com>
2024-04-12 16:09:44 +0200

    span_one interactivity

--- src/routes/applet/linear_combinations/span_one/+page.svelte
+++ src/routes/applet/linear_combinations/span_one/+page.svelte
@@ -4,17 +4,23 @@
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Controls } from '$lib/utils/Controls';
 
+
+
+
   const vDir = new Vector3(3, 2, -3);
   const lineDir = vDir.clone().normalize().multiplyScalar(10);
   let controls = Controls.addSlider(vDir.length(), -9, 9, 0.5, PrimeColor.raspberry);
-  $: v = vDir.clone().normalize().multiplyScalar(controls[0]); // Direction of vector v
+
+  $: v = new Vector3(3, 2, -3).normalize().multiplyScalar(3); // Vector v;
+  $: vLen = Math.abs(controls[0]) < 1 ? (controls[0] < 0 ? -1 : 1) : controls[0];
+
 </script>
 
 <Canvas3D bind:controls>
-  <Vector3D direction={v} length={v.length()} color={PrimeColor.raspberry} />
+  <Vector3D direction={vDir} length={vLen} color={PrimeColor.raspberry} />
   <Latex3D
     latex={'\\mathbf{v}'}
-    position={v.clone().add(new Vector3(0, 0.4, 0))}
+    position={v.clone().normalize().multiplyScalar(controls[0]).add(new Vector3(0, 0.4, 0))}
     color={PrimeColor.raspberry}
   />
 
