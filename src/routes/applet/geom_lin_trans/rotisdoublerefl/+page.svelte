b9380258922bd2deb8b170208fcde0977ab1f622
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-04-22 13:08:50 +0200

    added v with Translated L_1 and L_2

--- src/routes/applet/geom_lin_trans/rotisdoublerefl/+page.svelte
+++ src/routes/applet/geom_lin_trans/rotisdoublerefl/+page.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-  import { Canvas2D, Draggable2D, InfiniteLine2D, Latex2D, Vector2D } from '$lib/d3-components';
+  import {
+    Canvas2D,
+    Draggable2D,
+    InfiniteLine2D,
+    Latex2D,
+    Point2D,
+    Vector2D
+  } from '$lib/d3-components';
   import Arc from '$lib/d3-components/Arc.svelte';
+  import Angle from '$lib/d3-components/Angle.svelte';
   import { VECTOR_WIDTH } from '$lib/utils/AttributeDimensions';
   import { Formula, Formulas } from '$lib/utils/Formulas';
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Vector2 } from 'three';
 
   let dir_La = new Vector2(3, 2);
-  let dir_Lb = new Vector2(1, 3);
+  let dir_Lb = new Vector2(2, 3);
   let v = new Vector2(-3, -1);
 
   $: angle_a = dir_La.angle();
@@ -18,6 +26,9 @@
 
   $: angle_1 = Math.min(angle_a, angle_b);
   $: angle_2 = Math.max(angle_a, angle_b);
+
+  $: T_L1 = v.clone().rotateAround(new Vector2(0, 0), angle_1);
+  $: T_L2 = v.clone().rotateAround(new Vector2(0, 0), angle_2);
 
   function getFormulas(angle_1: number, angle_2: number) {
     const phiDiv2 = angle_1 / Math.PI;
@@ -45,17 +56,6 @@
     color={angle_a < angle_b ? PrimeColor.blue : PrimeColor.cyan}
   />
 
-  <!-- V -->
-  <Draggable2D id="v" snap bind:position={v} color={PrimeColor.raspberry} />
-  <Latex2D latex={'\\mathbf{v}'} position={v} extend={0.5} color={PrimeColor.raspberry} />
-  <Vector2D
-    radius={VECTOR_WIDTH / 2}
-    direction={v}
-    length={v.length()}
-    isDashed
-    color={PrimeColor.black}
-  />
-
   <!-- L1 -->
   <InfiniteLine2D direction={dir_L1} color={PrimeColor.cyan} />
   <Latex2D
@@ -77,7 +77,7 @@
   <!-- ARCS -->
   <Arc points={[new Vector2(1, 0), dir_L1]} distance={1.2} />
   <Latex2D
-    latex={'\\theta / 2'}
+    latex={'\\phi / 2'}
     position={dir_L1.clone().add(new Vector2(1, 0)).normalize().multiplyScalar(1.5)}
   />
 
@@ -85,5 +85,52 @@
   <Latex2D
     latex={'\\theta / 2'}
     position={dir_L2.clone().add(dir_L1).normalize().multiplyScalar(1.5)}
+  />
+
+  <!-- V -->
+  <Draggable2D id="v" snap bind:position={v} color={PrimeColor.raspberry} />
+  <Latex2D latex={'\\mathbf{v}'} position={v} extend={0.5} color={PrimeColor.raspberry} />
+  <Vector2D
+    direction={v}
+    length={v.length()}
+    radius={VECTOR_WIDTH / 2}
+    color={PrimeColor.black}
+    isDashed
+  />
+
+  <!-- T_L1(V) v + angle_1 -->
+  <Point2D position={T_L1} />
+  <Vector2D
+    origin={v}
+    direction={T_L1.clone().sub(v)}
+    length={T_L1.clone().sub(v).length()}
+    radius={VECTOR_WIDTH / 2}
+    color={PrimeColor.yellow}
+    isDashed
+  />
+
+  <!-- T_L2(T_L1(V)) v + angle_1 + angle_2 -->
+  <Point2D position={T_L2} />
+  <Vector2D
+    direction={T_L2}
+    length={T_L2.length()}
+    radius={VECTOR_WIDTH / 2}
+    color={PrimeColor.black}
+    isDashed
+  />
+  <Vector2D
+    origin={T_L1}
+    direction={T_L2.clone().sub(T_L1)}
+    length={T_L2.clone().sub(T_L1).length()}
+    radius={VECTOR_WIDTH / 2}
+    color={PrimeColor.yellow}
+    isDashed
+  />
+
+  <Angle
+    startAngle={v.angle()}
+    endAngle={v.angle() + angle_b}
+    distance={1}
+    color={PrimeColor.darkGreen}
   />
 </Canvas2D>
