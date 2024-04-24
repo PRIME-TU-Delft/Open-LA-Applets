1ce47540f6c337987e8db7ccf8e11fa8447e5493
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-04-24 12:42:43 +0200

    Fix vector length calculation in span_one and showFormulasDefault for span_three

--- src/routes/applet/linear_combinations/span_one/+page.svelte
+++ src/routes/applet/linear_combinations/span_one/+page.svelte
@@ -7,11 +7,15 @@
   const vDir = new Vector3(3, 2, -3);
   const lineDir = vDir.clone().normalize().multiplyScalar(10);
   let controls = Controls.addSlider(vDir.length(), -9, 9, 0.5, PrimeColor.raspberry);
-  $: v = vDir.clone().normalize().multiplyScalar(controls[0]); // Direction of vector v
+
+  // Do not allow the vector to be zero, otherwise the direction is not defined
+  $: vLength = Math.abs(controls[0]) <= 0.0001 ? 0.5 : controls[0];
+
+  $: v = vDir.clone().normalize().multiplyScalar(vLength); // Direction of vector v
 </script>
 
 <Canvas3D bind:controls>
-  <Vector3D direction={v} length={v.length()} color={PrimeColor.raspberry} />
+  <Vector3D alwaysOnTop direction={v} length={v.length()} color={PrimeColor.raspberry} />
   <Latex3D
     latex={'\\mathbf{v}'}
     position={v.clone().add(new Vector3(0, 0.4, 0))}
@@ -21,7 +25,7 @@
   <Line3D points={[lineDir.clone().multiplyScalar(-1), lineDir]} color={PrimeColor.darkGreen} />
   <Latex3D
     latex={'Span\\{\\mathbf{v}\\}'}
-    position={lineDir.add(new Vector3(0, 0.4, 0))}
+    position={lineDir.clone().add(new Vector3(0, 0.4, 0))}
     color={PrimeColor.darkGreen}
   />
   <Axis3D />
