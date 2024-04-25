1fd75d8aef8c9ba02d6b8d08e078f9e8796d6c7d
PHengst <paulinehengst1@gmail.com>
2024-04-25 15:01:08 +0200

    move latex funct to utils

--- src/lib/components/Latex.svelte
+++ src/lib/components/Latex.svelte
@@ -1,45 +1,12 @@
 <script lang="ts">
-  import katex from 'katex';
   import { primeColorArray } from '$lib/utils/PrimeColors';
+  import { convertLatex } from '$lib/utils/LatexFormat';
 
   export let latex = '';
   export let params: (string | number)[] = [];
   export let colors: string[] = primeColorArray;
 
-  let str = '';
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  function convertLatex(_: string) {
-    let input = latex;
-
-    try {
-      // Replace the parameters in the string with the values given by the user
-      for (let i = 0; i < params.length; i++) {
-        const value = parseFloat('' + params[i]).toFixed(2) || params[i];
-
-        input = input.replaceAll(
-          '\\$' + i,
-          `\\htmlStyle{color: ${colors[i % colors.length]};}{${value}}`
-        );
-      }
-
-      // For reference: https://katex.org/docs/options.html
-      str = katex.renderToString(input, {
-        displayMode: true,
-        leqno: false,
-        fleqn: false,
-        throwOnError: true,
-        output: 'html',
-        trust: true,
-        strict: false
-      });
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (e: any) {
-      str = e.message;
-    }
-  }
-
-  $: convertLatex(params.join(''));
+  $: str = convertLatex(latex, params, colors);
 </script>
 
 <div class="latex">
--- src/lib/utils/LatexFormat.ts
+++ src/lib/utils/LatexFormat.ts
@@ -1,4 +1,5 @@
 import type { Vector3 } from 'three';
+import katex from 'katex';
 
 export function bVector(v: Vector3) {
   return `\\begin{bmatrix} ${v.z} \\\\ ${v.x} \\\\ ${v.y} \\end{bmatrix}`;
@@ -6,4 +7,38 @@ export function bVector(v: Vector3) {
 
 export function color(param: number | string, color: string) {
   return `\\htmlStyle{color: ${color};}{${param}}`;
+}
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function convertLatex(latex: string, params: (string | number)[], colors: string[]) {
+  let str = '';
+  let input = latex;
+
+  try {
+    // Replace the parameters in the string with the values given by the user
+    for (let i = 0; i < params.length; i++) {
+      const value = parseFloat('' + params[i]).toFixed(2) || params[i];
+
+      input = input.replaceAll(
+        '\\$' + i,
+        `\\htmlStyle{color: ${colors[i % colors.length]};}{${value}}`
+      );
+    }
+
+    // For reference: https://katex.org/docs/options.html
+    str = katex.renderToString(input, {
+      displayMode: true,
+      leqno: false,
+      fleqn: false,
+      throwOnError: true,
+      output: 'html',
+      trust: true,
+      strict: false
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    str = e.message;
+  }
+  return str;
 }
