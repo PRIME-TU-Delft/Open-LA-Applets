77df9ec2ad5148618d297c2dbfbc227d70694006
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-04-25 12:40:43 +0200

    re-add csf

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,13 @@
 import type { StorybookConfig } from '@storybook/sveltekit';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx|svelte)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-svelte-csf'
   ],
   framework: {
     name: '@storybook/sveltekit',
