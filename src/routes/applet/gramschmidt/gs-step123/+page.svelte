156be6ce4082facafcf4325fe41571c738683aca
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-05-29 20:28:04 +0200

    fix jarno issue

--- src/routes/applet/gramschmidt/gs-step123/+page.svelte
+++ src/routes/applet/gramschmidt/gs-step123/+page.svelte
@@ -2,7 +2,6 @@
   import { Axis3D, Latex3D, Vector3D, Canvas3D } from '$lib/threlte-components';
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Vector3 } from 'three';
-  import type { Canvas3DProps } from '$lib/threlte-components';
   
   const v1 = new Vector3(-1, 1, 2); // green vector
 </script>
@@ -15,8 +14,9 @@
   <Vector3D direction={v1} color={PrimeColor.darkGreen} length={v1.length()} />
   <Latex3D position={v1} color={PrimeColor.darkGreen} latex={'V1'} />
 
-  <svelte:fragment slot="splitCanvas">
+  <svelte:fragment slot="splitCanvas3d">
     <Vector3D direction={v1} color={PrimeColor.darkGreen} length={v1.length()} />
     <Latex3D position={v1} color={PrimeColor.darkGreen} latex={'V1'} />
+    <Axis3D showNumbers />
   </svelte:fragment>
 </Canvas3D>
