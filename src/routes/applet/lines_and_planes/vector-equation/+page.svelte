c86fdd3ff4b93372b874431222dad2544c69319e
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-08-21 13:01:34 +0200

    make v0 & u draggables and add formulas

--- src/routes/applet/lines_and_planes/vector-equation/+page.svelte
+++ src/routes/applet/lines_and_planes/vector-equation/+page.svelte
@@ -1,24 +1,48 @@
 <script lang="ts">
+  import { Controls } from '$lib/controls/Controls';
   import { Draggable } from '$lib/controls/Draggables.svelte';
   import Canvas2D from '$lib/d3/Canvas2D.svelte';
   import InfiniteLine2D from '$lib/d3/InfiniteLine2D.svelte';
   import Latex2D from '$lib/d3/Latex2D.svelte';
   import Vector2D from '$lib/d3/Vector2D.svelte';
+  import { Formula, Formulas } from '$lib/utils/Formulas';
+  import { round } from '$lib/utils/MathLib';
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Vector2 } from 'three';
 
   const draggables = [
-    new Draggable(new Vector2(-2, 1), PrimeColor.darkGreen, 'u', Draggable.snapToGrid)
+    new Draggable(new Vector2(-2, 1), PrimeColor.darkGreen, 'u', Draggable.snapToGrid),
+    new Draggable(new Vector2(5, 0), PrimeColor.raspberry, 'v0', Draggable.snapToGrid)
   ];
 
-  const v0 = new Vector2(5, 0);
+  const controls = Controls.addSlider(1, -5, 5, 0.5, PrimeColor.darkGreen, 'k', (x) =>
+    round(x).toString()
+  );
+
+  const v0 = $derived(draggables[1].value);
   const u = $derived(draggables[0].value);
 
-  const v1 = $derived(v0.clone().add(u));
+  const v1 = $derived(v0.clone().add(u.clone().multiplyScalar(controls[0])));
   const dir_L = $derived(v1.clone().sub(v0.clone()));
+
+  const formulas = $derived.by(() => {
+    const f1 = new Formula('\\mathbf{v}_1 &= \\mathbf{v}_0 + k \\cdot \\mathbf{u}');
+    const f2 = new Formula(
+      '\\begin{bmatrix} \\$1 \\\\ \\$2 \\end{bmatrix} &= \\begin{bmatrix} \\$3  \\\\ \\$4 \\end{bmatrix} + \\$5 \\begin{bmatrix} \\$6 \\\\ \\$7 \\end{bmatrix}'
+    )
+      .addAutoParam(round(v1.x), PrimeColor.yellow)
+      .addAutoParam(round(v1.y), PrimeColor.yellow)
+      .addAutoParam(round(v0.x), PrimeColor.raspberry)
+      .addAutoParam(round(v0.y), PrimeColor.raspberry)
+      .addAutoParam(controls[0].toFixed(2), PrimeColor.darkGreen)
+      .addAutoParam(round(u.x), PrimeColor.darkGreen)
+      .addAutoParam(round(u.y), PrimeColor.darkGreen);
+
+    return new Formulas(f1, f2).align();
+  });
 </script>
 
-<Canvas2D {draggables}>
+<Canvas2D {controls} {draggables} {formulas}>
   <!-- Line L -->
   <InfiniteLine2D origin={v0} direction={dir_L} color={PrimeColor.cyan} />
 
@@ -53,7 +77,6 @@
   </Vector2D>
 
   <!-- U -->
-  <!-- <Draggable2D snap id="u" bind:position={u} color={PrimeColor.darkGreen} /> -->
   <Vector2D direction={u} length={u.length()} color={PrimeColor.darkGreen}>
     {#snippet children(endPoint)}
       <Latex2D
@@ -65,7 +88,12 @@
     {/snippet}
   </Vector2D>
 
-  <Vector2D origin={v0} direction={u} length={u.length()} color={PrimeColor.darkGreen}>
+  <Vector2D
+    origin={v0}
+    direction={u}
+    length={u.length() * controls[0]}
+    color={PrimeColor.darkGreen}
+  >
     {#snippet children(endPoint)}
       <Latex2D
         latex={'\\mathbf{u}'}
