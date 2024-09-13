5b6240be643c17185588212934f0eab3ac0b7cd2
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-09-13 11:18:57 +0200

    Update applet with teachers/writers

--- src/routes/applet/eigenvalue_eigenvector/no_eigenvector/+page.svelte
+++ src/routes/applet/eigenvalue_eigenvector/no_eigenvector/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { Controls } from '$lib/controls/Controls';
+  import type { SlideShowSteps } from '$lib/controls/SlideShow.svelte';
   import Canvas2D from '$lib/d3/Canvas2D.svelte';
   import Latex2D from '$lib/d3/Latex2D.svelte';
   import Vector2D from '$lib/d3/Vector2D.svelte';
@@ -8,17 +9,50 @@
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Matrix3, Vector2, Vector3 } from 'three';
 
-  const controls = Controls.addSlider(
-    0,
-    0,
-    2,
-    1 / 20,
-    PrimeColor.raspberry,
-    'Angle',
-    (x) => round(x, 2).toString() + 'π'
-  );
+  const defaultState = {
+    angle: 0,
+    avLabel: 'A\\mathbf{w}\\neq\\lambda\\mathbf{w}'
+  };
 
-  const A = new Matrix3(5 / 3, -2 / 3, 0, -1 / 3, 4 / 3, 0, 0, 0, 0);
+  const steps: SlideShowSteps<typeof defaultState> = [
+    (t, state) => {
+      state.angle = state.angle + 0.15 * t;
+
+      if (t > 0.9) state.avLabel = 'A\\mathbf{w}=3\\mathbf{w}';
+      else 'A\\mathbf{w}\\neq\\lambda\\mathbf{w}';
+
+      return { state, labelNext: 'First eigenvector', labelPrev: 'No eigenvector' };
+    },
+    (t, state) => {
+      state.angle = state.angle + 0.7 * t;
+
+      if (t > 0.9) state.avLabel = 'A\\mathbf{w}=-1\\mathbf{w}';
+      else 'A\\mathbf{w}\\neq\\lambda\\mathbf{w}';
+
+      return { state, labelNext: 'Second eigenvector', labelPrev: 'First eigenvector' };
+    },
+    (t, state) => {
+      state.angle = state.angle + 0.3 * t;
+
+      if (t > 0.9) state.avLabel = 'A\\mathbf{w}=3\\mathbf{w}';
+      else 'A\\mathbf{w}\\neq\\lambda\\mathbf{w}';
+
+      return { state, labelNext: 'Third eigenvector', labelPrev: 'Second eigenvector' };
+    },
+    (t, state) => {
+      state.angle = state.angle + 0.7 * t;
+
+      if (t > 0.9) state.avLabel = 'A\\mathbf{w}=-1\\mathbf{w}';
+      else 'A\\mathbf{w}\\neq\\lambda\\mathbf{w}';
+
+      return { state, labelNext: 'Fourth eigenvector', labelPrev: 'Third eigenvector' };
+    }
+  ];
+
+  const controls = Controls.addSlideShow(defaultState, steps, 'No eigenvector');
+  const state = $derived(controls[0]);
+
+  const A = new Matrix3(1, 4, 0, 1, 1, 0, 0, 0, 0);
 
   function rotationMatrix(angle: number) {
     const c = Math.cos(angle);
@@ -26,72 +60,29 @@
     return new Matrix3(c, -s, 0, s, c, 0, 0, 0, 0);
   }
 
-  const v1 = $derived.by(() => {
-    const rotation = rotationMatrix(controls[0] * Math.PI);
-    const v = new Vector3(2, 1, 0);
-    v.applyMatrix3(rotation);
+  const w = $derived.by(() => {
+    const rotation = rotationMatrix(state.angle * Math.PI);
+    const w = new Vector3(2, 0, 0);
+    w.applyMatrix3(rotation);
 
-    return new Vector2(v.x, v.y);
-  });
-  const v2 = $derived.by(() => {
-    const rotation = rotationMatrix(controls[0] * Math.PI);
-    const v = new Vector3(1, 0, 0);
-    v.applyMatrix3(rotation);
-
-    return new Vector2(v.x, v.y);
+    return new Vector2(w.x, w.y);
   });
 
-  function applyMatrixA(v: Vector2) {
-    const v3 = new Vector3(v.x, v.y, 0);
-    v3.applyMatrix3(A);
-    return new Vector2(v3.x, v3.y);
-  }
-
-  const Av1 = $derived(applyMatrixA(v1));
-  const Av2 = $derived(applyMatrixA(v2));
-
-  const Av1Label = $derived.by(() => {
-    const solutions = [
-      { angle: 0.1, lambda: 1 },
-      { angle: 0.7, lambda: 2 },
-      { angle: 1.1, lambda: 1 },
-      { angle: 1.7, lambda: 2 }
-    ];
-
-    const solution = solutions.find((v) => controls[0] === v.angle);
-
-    if (solution) {
-      return 'A\\mathbf{v_2}=' + solution.lambda + '\\mathbf{v_2}';
-    }
-
-    return 'A\\mathbf{v_2}\\neq\\lambda\\mathbf{v_2}';
-  });
-  const Av2Label = $derived.by(() => {
-    const solutions = [
-      { angle: 0.25, lambda: 1 },
-      { angle: 0.85, lambda: 2 },
-      { angle: 1.25, lambda: 1 },
-      { angle: 1.85, lambda: 2 }
-    ];
-
-    const solution = solutions.find((v) => controls[0] === v.angle);
-
-    if (solution) {
-      return 'A\\mathbf{v_2}=' + solution.lambda + '\\mathbf{v_2}';
-    }
-
-    return 'A\\mathbf{v_2}\\neq\\lambda\\mathbf{v_2}';
+  const Aw = $derived.by(() => {
+    const w_temp = new Vector3(w.x, w.y, 0);
+    w_temp.applyMatrix3(A);
+    return new Vector2(w_temp.x, w_temp.y);
   });
 
   const formulas = $derived.by(() => {
     const f1 = new Formula('A = \\frac{1}{3}\\begin{bmatrix} 5 & -2 \\\\ -1 & 4 \\end{bmatrix}');
     const f2 = new Formula(
-      'v_1 = \\begin{bmatrix} \\$1 \\\\ \\$2 \\end{bmatrix} \\quad v_2 = \\begin{bmatrix} \\$3 \\\\ \\$4 \\end{bmatrix}'
+      'A\\mathbf{w} = \\begin{bmatrix} \\$1 \\\\ \\$2 \\end{bmatrix} \\quad \\mathbf{w} = \\begin{bmatrix} \\$3 \\\\ \\$4 \\end{bmatrix}'
     )
-      .addAutoParam(round(v1.x, 1), PrimeColor.raspberry)
-      .addAutoParam(round(v1.y, 1), PrimeColor.raspberry)
-      .addAutoParam(round(v2.x, 1), PrimeColor.raspberry)
-      .addAutoParam(round(v2.y, 1), PrimeColor.raspberry);
+      .addAutoParam(round(Aw.x, 1), PrimeColor.blue)
+      .addAutoParam(round(Aw.y, 1), PrimeColor.blue)
+      .addAutoParam(round(w.x, 1), PrimeColor.raspberry)
+      .addAutoParam(round(w.y, 1), PrimeColor.raspberry);
 
     return [f1, f2];
   });
@@ -99,42 +90,21 @@
 
 <Canvas2D {controls} {formulas}>
   <!-- V1 -->
-  <Vector2D direction={Av1} length={Av1.length()} color={PrimeColor.blue} />
+  <Vector2D direction={Aw} length={Aw.length()} color={PrimeColor.blue} />
   <Latex2D
     extend={0.5}
     offset={new Vector2(-0.25, 0.25)}
-    latex={Av1Label}
-    position={Av1}
+    latex={state.avLabel}
+    position={Aw}
     color={PrimeColor.blue}
   />
 
-  <Vector2D direction={v1} length={v1.length()} color={PrimeColor.raspberry} />
+  <Vector2D direction={w} length={w.length()} color={PrimeColor.raspberry} />
   <Latex2D
     offset={new Vector2(-0.25, 0)}
     extend={0.5}
-    latex={'\\mathbf{v_1}'}
-    position={v1}
+    latex={'\\mathbf{w}'}
+    position={w}
     color={PrimeColor.raspberry}
   />
-
-  <!-- V2 -->
-  {#snippet splitCanvas2DChildren()}
-    <Vector2D direction={Av2} length={Av2.length()} color={PrimeColor.blue} />
-    <Latex2D
-      extend={0.5}
-      offset={new Vector2(-0.25, 0.25)}
-      latex={Av2Label}
-      position={Av2}
-      color={PrimeColor.blue}
-    />
-
-    <Vector2D direction={v2} length={v2.length()} color={PrimeColor.raspberry} />
-    <Latex2D
-      offset={new Vector2(-0.25, 0)}
-      extend={0.5}
-      latex={'\\mathbf{v_2}'}
-      position={v2}
-      color={PrimeColor.raspberry}
-    />
-  {/snippet}
 </Canvas2D>
