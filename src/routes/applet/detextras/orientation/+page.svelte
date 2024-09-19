c0eac1e2cbfa4ecc94023096c28a49c4ac0e64fd
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-09-19 14:43:02 +0200

    Make orientation applet look more pleasing

--- src/routes/applet/detextras/orientation/+page.svelte
+++ src/routes/applet/detextras/orientation/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { Controls } from '$lib/controls/Controls';
   import Angle2D from '$lib/d3/Angle2D.svelte';
   import Canvas2D from '$lib/d3/Canvas2D.svelte';
@@ -6,7 +6,7 @@
   import Vector2D from '$lib/d3/Vector2D.svelte';
   import { Formula } from '$lib/utils/Formulas';
   import { PrimeColor } from '$lib/utils/PrimeColors';
-  import { Vector2 } from 'three';
+  import { Matrix3, Vector2, Vector3 } from 'three';
 
   const controls = Controls.addSlider(0, 0, 360, 10, PrimeColor.cyan, { loop: true }).addToggle(
     false,
@@ -24,22 +24,60 @@
     return [f1, f2];
   });
 
-  const transformMatrix = $derived(controls[1] ? 'matrix(3 1 1 3 0 0)' : 'matrix(1 3 3 1 0 0)');
+  const transformMatrix = $derived(
+    controls[1] ? new Matrix3(3, 1, 0, 1, 3, 0, 0, 0, 1) : new Matrix3(1, 3, 0, 3, 1, 0, 0, 0, 1)
+  );
+
+  function transformVector(v: Vector2, angle: number): Vector2 {
+    let temp = new Vector3(v.x, v.y, 0);
+
+    temp = temp.applyAxisAngle(new Vector3(0, 0, 1), (angle * Math.PI) / 180);
+    temp = temp.applyMatrix3(transformMatrix);
+
+    return new Vector2(temp.x, temp.y);
+  }
+
+  const v1 = new Vector2(1, 0);
+  const v2 = new Vector2(Math.cos(Math.PI / 4), Math.sin(Math.PI / 4));
+  const tv1 = $derived(transformVector(v1, controls[0]));
+  const tv2 = $derived(transformVector(v2, controls[0]));
+
+  const transformMatrixStr = $derived(controls[1] ? 'matrix(3 1 1 3 0 0)' : 'matrix(1 3 3 1 0 0)');
 </script>
 
 <Canvas2D {formulas} showFormulasDefault cameraZoom={3} {controls}>
+  <Circle2D color={PrimeColor.darkGreen} />
+
   {@render scene()}
 
+  <g transform="rotate({controls[0]})">
+    <Vector2D direction={v1} length={v1.length()} color={PrimeColor.cyan} radius={0.04} />
+    <Vector2D direction={v2} length={v2.length()} color={PrimeColor.cyan} radius={0.04} />
+  </g>
+
   {#snippet splitCanvas2DChildren()}
-    <g transform={transformMatrix}>
+    <g transform="rotate(45)">
+      <ellipse
+        cx="0"
+        cy="0"
+        rx="4"
+        ry="2"
+        fill="none"
+        stroke={PrimeColor.darkGreen}
+        stroke-width={0.07}
+      />
+    </g>
+
+    <g transform={transformMatrixStr}>
       {@render scene()}
     </g>
+
+    <Vector2D direction={tv1} length={tv1.length()} color={PrimeColor.cyan} radius={0.1} />
+    <Vector2D direction={tv2} length={tv2.length()} color={PrimeColor.cyan} radius={0.1} />
   {/snippet}
 </Canvas2D>
 
 {#snippet scene()}
-  <Circle2D color={PrimeColor.darkGreen} />
-
   <g transform="rotate({controls[0]})">
     <Angle2D
       startAngle={0}
@@ -49,7 +87,5 @@
       width={0.015}
       hasHead
     />
-    <Vector2D direction={new Vector2(1, 0)} length={1} color={PrimeColor.cyan} />
-    <Vector2D direction={new Vector2(1, 1)} length={1} color={PrimeColor.cyan} />
   </g>
 {/snippet}
