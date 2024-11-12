6ab4c9820d4f1e59555119750769c4dcb5c88dc6
Abel de Bruijn <abeldebruijn@hotmail.com>
2024-11-12 13:09:54 +0100

    Add injectivity and surjectivity example and remove shadow trail components

--- src/routes/applet/injectivity_and_surjectivity/injsurj-injex-example3/+page.svelte
+++ src/routes/applet/injectivity_and_surjectivity/injsurj-injex-example3/+page.svelte
@@ -1,21 +1,46 @@
 <script lang="ts">
+  import { Controls } from '$lib/controls/Controls';
   import { Draggable } from '$lib/controls/Draggables.svelte';
   import Canvas2D from '$lib/d3/Canvas2D.svelte';
   import Latex2D from '$lib/d3/Latex2D.svelte';
+  import Point2D from '$lib/d3/Point2D.svelte';
   import Vector2D from '$lib/d3/Vector2D.svelte';
+  import { confettiState } from '$lib/stores/confetti.svelte';
   import { Formula } from '$lib/utils/Formulas';
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Vector2 } from 'three';
 
-  const draggables = [
-    new Draggable(new Vector2(2, 1), PrimeColor.blue, '\\mathbf{v_1}'),
-    new Draggable(new Vector2(-1, 2), PrimeColor.raspberry, '\\mathbf{v_2}')
-  ];
+  function randomVector() {
+    const x = Math.round((Math.random() * 6 - 3) * 2.5) / 2;
+    const y = Math.round((Math.random() * 6 - 3) * 2.5) / 2;
+
+    return new Vector2(x, y);
+  }
+
+  // Target to reach
+  let target = $state(randomVector());
+
+  const controls = Controls.addButton('New target', PrimeColor.blue, () => {
+    target = randomVector();
+  });
 
   function transform(vector: Vector2) {
     // Transform with the matrix [1 -1; 1 1]
     return new Vector2(vector.x - vector.y, vector.x + vector.y);
   }
+
+  function releaseFunction(vector: Vector2) {
+    if (vector.distanceTo(target) < 0.5) {
+      confettiState.center(1000);
+      vector = target.clone();
+    }
+
+    return vector;
+  }
+
+  const draggables = [
+    new Draggable(new Vector2(2, 1), PrimeColor.blue, '\\mathbf{v_1}', (v) => v, releaseFunction)
+  ];
 
   const formulas = $derived.by(() => {
     const f1 = new Formula(
@@ -29,6 +54,7 @@
 <Canvas2D
   {draggables}
   {formulas}
+  {controls}
   showFormulasDefault
   cameraZoom={1.75}
   cameraPosition={new Vector2(0, 2)}
@@ -43,6 +69,14 @@
   {/each}
 
   {#snippet splitCanvas2DChildren()}
+    <Point2D position={transform(target)} color={PrimeColor.raspberry} pulse />
+    <Latex2D
+      latex={'\\text{Target}'}
+      position={transform(target)}
+      offset={new Vector2(-0.55, -0.2)}
+      extend={0.3}
+    />
+
     {#each draggables as draggable, index}
       {@const transformed = transform(draggable.position)}
       <Vector2D direction={transformed} length={transformed.length()} color={draggable.color} />
