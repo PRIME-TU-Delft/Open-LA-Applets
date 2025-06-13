9c2069f2da07c744043be2fc1f9444c724d3d3ca
Bartek <bewuwy@gmail.com>
2025-06-13 11:06:51 +0200

    Changed formula and names to be complex instead of vectors

--- src/routes/applet/complex_basics/complex_addition/+page.svelte
+++ src/routes/applet/complex_basics/complex_addition/+page.svelte
@@ -7,8 +7,8 @@
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Vector2 } from 'three';
 
-  let d1 = new Draggable(new Vector2(1, 2), PrimeColor.blue, 'v_1', Draggable.snapToGrid);
-  let d2 = new Draggable(new Vector2(2, 0), PrimeColor.darkGreen, 'v_2', Draggable.snapToGrid);
+  let d1 = new Draggable(new Vector2(1, 2), PrimeColor.blue, 'z_1', Draggable.snapToGrid);
+  let d2 = new Draggable(new Vector2(2, 0), PrimeColor.darkGreen, 'z_2', Draggable.snapToGrid);
 
   let draggables = [d1, d2];
 
@@ -18,26 +18,27 @@
   let v2_dashed = $derived(v_result.clone().sub(d1.position));
 
   const formulas = $derived.by(() => {
-    const f1 = new Formula(
-      '\\begin{bmatrix}\\$1 \\\\ \\$2 \\end{bmatrix} + \\begin{bmatrix}\\$3 \\\\ \\$4 \\end{bmatrix} = \\begin{bmatrix}\\$5 \\\\ \\$6 \\end{bmatrix}'
-    )
+    const f1 = new Formula('(\\$1 \\$7 \\$2 i) + (\\$3 \\$8 \\$4 i) = (\\$5 \\$9 \\$6 i)')
       .addAutoParam(Math.round(d1.position.x), PrimeColor.blue)
-      .addAutoParam(Math.round(d1.position.y), PrimeColor.blue)
+      .addAutoParam(Math.abs(Math.round(d1.position.y)), PrimeColor.blue)
       .addAutoParam(Math.round(d2.position.x), PrimeColor.darkGreen)
-      .addAutoParam(Math.round(d2.position.y), PrimeColor.darkGreen)
+      .addAutoParam(Math.abs(Math.round(d2.position.y)), PrimeColor.darkGreen)
       .addAutoParam(Math.round(v_result.x), PrimeColor.raspberry)
-      .addAutoParam(Math.round(v_result.y), PrimeColor.raspberry);
+      .addAutoParam(Math.abs(Math.round(v_result.y)), PrimeColor.raspberry)
+      .addAutoParam(d1.position.y >= 0 ? '+' : '-', PrimeColor.blue)
+      .addAutoParam(d2.position.y >= 0 ? '+' : '-', PrimeColor.darkGreen)
+      .addAutoParam(v_result.y >= 0 ? '+' : '-', PrimeColor.raspberry);
 
-    const f2 = new Formula('v_1 + v_2 = v_3');
+    const f2 = new Formula('z_1 + z_2 = z_3');
     return [f2, f1];
   });
 </script>
 
-<Canvas2D {draggables} {formulas} title="Vector addition">
+<Canvas2D {draggables} {formulas} title="Complex numbers addition">
   <Vector2D direction={d1.position} length={d1.position.length()} color={PrimeColor.blue} />
   <Vector2D direction={d2.position} length={d2.position.length()} color={PrimeColor.darkGreen} />
   <Vector2D direction={v_result} length={v_result.length()} color={PrimeColor.raspberry} />
-  <Latex2D latex={'v_3'} position={v_result} color={PrimeColor.raspberry} />
+  <Latex2D latex={'z_3'} position={v_result} color={PrimeColor.raspberry} />
 
   <Vector2D
     direction={v1_dashed}
