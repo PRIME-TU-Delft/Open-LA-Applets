3a4a63710d3431d94bc69216026863cb9e7f2bb9
Bartek <bewuwy@gmail.com>
2025-07-15 11:07:19 +0200

    removed setTemplate from PolarGrid docs

--- src/lib/d3/stories/Latex2D.stories.svelte
+++ src/lib/d3/stories/Latex2D.stories.svelte
@@ -1,4 +1,4 @@
-<script module>
+<script module lang="ts">
   import { defineMeta } from '@storybook/addon-svelte-csf';
   import Latex2D, { type Latex2DProps } from '../Latex2D.svelte';
 
--- src/lib/d3/stories/PolarGrid.stories.svelte
+++ src/lib/d3/stories/PolarGrid.stories.svelte
@@ -1,5 +1,5 @@
-<script module>
-  import { defineMeta, setTemplate } from '@storybook/addon-svelte-csf';
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
   import PolarGrid from '../PolarGrid.svelte';
 
   const { Story } = defineMeta({
@@ -13,8 +13,6 @@
   import Canvas2D from '../Canvas2D.svelte';
   import type { PolarGridProps } from '../PolarGrid.svelte';
 
-  setTemplate(template as Snippet<[Partial<PolarGridProps>]>);
-
   let canvasProps = { customAxis: true };
 </script>
 
@@ -27,16 +25,16 @@
 {/snippet}
 
 <!-- Polar grid with default parameters. This story demonstrates a polar grid with a default angle step of 30 degrees. -->
-<Story name="Default" args={{}} />
+<Story name="Default" args={{}} {template} />
 
 <!-- Polar grid without ticks and radii numbers. This story demonstrates a polar grid without any ticks on the x-axis. -->
-<Story name="Without radii ticks" args={{ showRadiiTicks: false }} />
+<Story name="Without radii ticks" args={{ showRadiiTicks: false }} {template} />
 
 <!-- Polar grid with angle ticks enabled. This story demonstrates how to show the angle sizes next to the angled grid lines. The angle labels will be further away from the origin based on the number of angled lines (angleStep). -->
-<Story name="With angle ticks" args={{ showAngleTicks: true }} />
+<Story name="With angle ticks" args={{ showAngleTicks: true }} {template} />
 
 <!-- Polar grid with changed angleStep. This story demonstrates a polar grid with a custom angle step of 45 degrees, showcasing how you can customize the grid lines. -->
-<Story name="With 45 degree step" args={{ angleStep: 45 }} />
+<Story name="With 45 degree step" args={{ angleStep: 45 }} {template} />
 
 <!-- Polar grid with some highlighted radii. This story demonstrates a polar grid with some circles highlighted, showcasing how you can mark some distances as "special". -->
-<Story name="With highlighted radii" args={{ highlightRadii: [1, 3, 4, 6] }} />
+<Story name="With highlighted radii" args={{ highlightRadii: [1, 3, 4, 6] }} {template} />
