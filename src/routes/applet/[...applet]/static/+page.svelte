e7341d003f82f0ae22806fab51d8a7b09afae437
bewu <bewuwy@gmail.com>
2025-09-26 17:15:03 +0200

    added a static subpage for applets

---
+++ src/routes/applet/[...applet]/static/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from '$app/state';
+  import { getScreenshotName } from '$lib/screenshots/utils';
+
+  const applet = $derived(page.params.applet);
+</script>
+
+<div class="flex h-full w-full justify-center">
+  <img src={'/screenshots/' + getScreenshotName(applet)} alt={applet} class="h-full w-auto" />
+</div>
