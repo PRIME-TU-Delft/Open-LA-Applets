c310a79a373728da5cf95b4844a32f6ab413a2e3
bewu <bewuwy@gmail.com>
2025-09-26 17:22:26 +0200

    added fallback for static pages

--- src/routes/applet/[...applet]/static/+page.svelte
+++ src/routes/applet/[...applet]/static/+page.svelte
@@ -3,8 +3,21 @@
   import { getScreenshotName } from '$lib/screenshots/utils';
 
   const applet = $derived(page.params.applet);
+
+  let found: boolean = $state(true);
 </script>
 
-<div class="flex h-full w-full justify-center">
-  <img src={'/screenshots/' + getScreenshotName(applet)} alt={applet} class="h-full w-auto" />
+<div class="flex h-full w-full content-center justify-center">
+  {#if found}
+    <img
+      src={'/screenshots/' + getScreenshotName(applet)}
+      alt={applet}
+      class="h-full w-auto"
+      onerror={() => {
+        found = false;
+      }}
+    />
+  {:else}
+    <span>Static image for <b>{applet}</b> not found</span>
+  {/if}
 </div>
