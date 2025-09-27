d0d03f325009dadcd00d60d177f73ba8d5139095
bewu <bewuwy@gmail.com>
2025-09-27 16:44:50 +0200

    add an option to have a QR code

--- src/routes/applet/[...applet]/static/+page.svelte
+++ src/routes/applet/[...applet]/static/+page.svelte
@@ -1,23 +1,45 @@
 <script lang="ts">
   import { page } from '$app/state';
   import { getScreenshotName } from '$lib/screenshots/utils';
+  import { qr } from '@svelte-put/qr/svg';
+
+  const queryParams = new URLSearchParams(page.url.searchParams);
+  const showQR = queryParams.get('qr') === 'true';
 
   const applet = $derived(page.params.applet);
 
   let found: boolean = $state(true);
 </script>
 
-<div class="flex h-full w-full content-center justify-center">
-  {#if found}
-    <img
-      src={'/screenshots/' + getScreenshotName(applet)}
-      alt={applet}
-      class="h-full w-auto"
-      onerror={() => {
-        found = false;
-      }}
-    />
-  {:else}
-    <span>Static image for <b>{applet}</b> not found</span>
-  {/if}
+<div class="h-full w-full content-center justify-center">
+  <div class="relative h-auto w-full">
+    {#if found}
+      <img
+        src={'/screenshots/' + getScreenshotName(applet)}
+        alt={applet}
+        class="w-full"
+        onerror={() => {
+          found = false;
+        }}
+      />
+      {#if showQR}
+        <div
+          class="absolute right-0 bottom-0 h-auto w-48 rounded-lg bg-gradient-to-bl from-blue-400 to-blue-500 p-2 transition-all duration-500"
+        >
+          <div class="rounded-lg bg-white">
+            <span class="ml-1">Use interactively:</span>
+            <svg
+              use:qr={{
+                data: 'https://openla.ewi.tudelft.nl/applet/' + applet,
+                logo: '/primeLogo-black.svg',
+                shape: 'circle'
+              }}
+            />
+          </div>
+        </div>
+      {/if}
+    {:else}
+      <span>Static image for <b>{applet}</b> not found</span>
+    {/if}
+  </div>
 </div>
