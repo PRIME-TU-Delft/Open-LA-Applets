2afaeeba011cd91f610bfe785ab80ca89a640ed4
Bartek <bewuw@proton.me>
2025-12-28 21:19:00 +0100

    added placeholders to the function input

--- src/lib/components/controls/Function.svelte
+++ src/lib/components/controls/Function.svelte
@@ -9,10 +9,43 @@
   };
 
   let { func }: FunctionProps = $props();
+
+  function getRandomPlaceholder() {
+    const placeholders = [
+      '\\sin{(x)}',
+      '\\cos{(x)}',
+      'x^2 + 2x + 1',
+      '\\sqrt{x + 2}',
+      'e^{x}',
+      '\\log_{2}{x}',
+      '\\frac{1}{x}',
+      '\\tan{(x)}',
+      '|x-3|',
+      '\\frac{\\sin{x}}{x}',
+      '\\ln(x^2+1)',
+      '\\max(x, 5)',
+      '\\min(x^2, 10)',
+      '\\sqrt[3]{x}',
+      '\\arctan(x)',
+      '\\exp(-x^2)',
+      'x^{1/3}',
+      '\\cosh(x)'
+    ];
+
+    return placeholders[Math.floor(Math.random() * placeholders.length)] + '\\; ...';
+  }
+
+  let placeholder = $state(getRandomPlaceholder());
+
+  $effect(() => {
+    if (!func.functionString) {
+      placeholder = getRandomPlaceholder();
+    }
+  });
 </script>
 
 {#if func.label}
   <Latex latex={func.label + ':'} color={func.color.toString()} />
 {/if}
 
-<MathInput class="w-[150px]" error={func.isError} bind:value={func.functionString} />
+<MathInput class="w-[150px]" {placeholder} error={func.isError} bind:value={func.functionString} />
