5b221e5557d237c2c0c557422a9a18d7a1f6e094
Bartek <bewuw@proton.me>
2026-02-16 12:36:07 +0100

    moved to the alternative style

--- src/routes/applet/calculus/riemann_sum/riemann_sum_2d/+page.svelte
+++ src/routes/applet/calculus/riemann_sum/riemann_sum_2d/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { Controls } from '$lib/controls/Controls';
   import Canvas2D from '$lib/d3/Canvas2D.svelte';
-  import Rect2D from '$lib/d3/Rect2D.svelte';
   import Point2D from '$lib/d3/Point2D.svelte';
   import { Formula, Formulas } from '$lib/utils/Formulas';
   import { round, roundString } from '$lib/utils/MathLib';
@@ -10,6 +9,7 @@
   import { Draggable } from '$lib/controls/Draggables.svelte';
   import ExplicitFunction2D from '$lib/d3/ExplicitFunction2D.svelte';
   import Latex2D from '$lib/d3/Latex2D.svelte';
+  import Parallelogram2D from '$lib/d3/Parallelogram2D.svelte';
 
   const methods = [
     'applets.calculus.riemann_sum.riemann_sum_2d.middle',
@@ -61,9 +61,9 @@
   };
 
   const func_display = '\\int_{\\$1}^{\\$2} f(x) \\, dx = \\$3';
+  const numRectangles = $derived(round(controls[1], 0));
 
   const formulas = $derived.by(() => {
-    const numRectangles = round(controls[1]);
     const dx = (xR - xL) / numRectangles;
     const integralResult = intFunc(xR) - intFunc(xL);
 
@@ -84,7 +84,6 @@
   });
 
   const rects = $derived.by(() => {
-    const numRectangles = controls[1];
     const dx = (xR - xL) / numRectangles;
     const newRects = [];
     for (let i = 0; i < numRectangles; i++) {
@@ -136,22 +135,15 @@
       const x1 = xL + i * dx;
       const x2 = x1 + dx;
       const y = func(x);
-      const color =
-        xR > xL
-          ? y >= 0
-            ? PrimeColor.darkGreen
-            : PrimeColor.raspberry
-          : y < 0
-            ? PrimeColor.darkGreen
-            : PrimeColor.raspberry;
+
       newRects.push({
-        points: [new Vector2(x1, Math.min(0, y)), new Vector2(x2, Math.max(0, y))] as [
-          Vector2,
-          Vector2
-        ],
+        points: [
+          new Vector2(x2, Math.min(0, y)),
+          new Vector2(x1, Math.min(0, y)),
+          new Vector2(x2, Math.max(0, y))
+        ] as [Vector2, Vector2, Vector2],
         height: y,
-        samplePosition: samplePosition,
-        color
+        samplePosition: samplePosition
       });
     }
     return newRects;
@@ -173,34 +165,39 @@
   <Latex2D latex="x" position={new Vector2(10.5, 0.55)} />
   <Latex2D latex="f(x)" position={new Vector2(0.25, 6.25)} />
 
-  <ExplicitFunction2D {func} color={PrimeColor.blue} stepSize={0.1} />
+  <ExplicitFunction2D
+    {func}
+    color={PrimeColor.blue}
+    stepSize={0.1}
+    integral={{ xLeft: xL, xRight: xR, fillStyle: 'full' }}
+  />
 
-  <g>
-    {#each rects as rect, index (index)}
-      <Rect2D points={rect.points} color={rect.color + '90'} />
-    {/each}
-    {#each rects as rect, index (index)}
-      <Point2D
-        position={rect.samplePosition}
-        color={PrimeColor.black}
-        radius={0.04}
-        text={`${round(rect.height, 2)}`}
-        showTextOnlyOnHover={true}
-        fontSize={0.5}
-        offset={Math.max(rect.points[0].y, rect.points[1].y) > 0
-          ? new Vector2(0, 0.3)
-          : new Vector2(0, -0.1)}
-      />
-    {/each}
-    {#each sampledPoints as point, index (index)}
-      <Point2D
-        position={point}
-        color={PrimeColor.orange}
-        radius={0.03}
-        text={`T_{${index + 1}}`}
-        fontSize={0.4}
-        offset={new Vector2(0, -0.05)}
-      />
-    {/each}
-  </g>
+  {#each rects as rect, index (index)}
+    <Parallelogram2D points={rect.points} color={PrimeColor.orange} fillStyle="dashed" />
+  {/each}
+
+  {#each rects as rect, index (index)}
+    <Point2D
+      position={rect.samplePosition}
+      color={PrimeColor.orange}
+      radius={0.06}
+      text={`${round(rect.height, 2)}`}
+      showTextOnlyOnHover={true}
+      fontSize={0.5}
+      offset={Math.max(rect.points[0].y, rect.points[1].y) > 0
+        ? new Vector2(0, 0.3)
+        : new Vector2(0, -0.1)}
+    />
+  {/each}
+
+  {#each sampledPoints as point, index (index)}
+    <Point2D
+      position={point}
+      color={PrimeColor.black}
+      radius={0.05}
+      text={`T_{${index + 1}}`}
+      fontSize={0.4}
+      offset={new Vector2(0, -0.05)}
+    />
+  {/each}
 </Canvas2D>
