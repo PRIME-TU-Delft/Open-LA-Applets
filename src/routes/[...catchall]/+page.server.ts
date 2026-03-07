597433161e9cf924c2ab1c0b38e45d8c7e85affa
Bartek <bewuw@proton.me>
2026-03-07 18:44:15 +0100

    format

--- src/routes/[...catchall]/+page.server.ts
+++ src/routes/[...catchall]/+page.server.ts
@@ -1,35 +1,34 @@
 import type { PageServerLoad } from './$types';
 
 type AppletUsageEntry = {
-    url: string;
-    title: string;
-    name: string;
-    source_files: string[];
+  url: string;
+  title: string;
+  name: string;
+  source_files: string[];
 };
 
 type BookAppletUsage = {
-    applets: AppletUsageEntry[];
+  applets: AppletUsageEntry[];
 };
 
 type AppletsUsageByBook = Record<string, BookAppletUsage>;
 
 export const load: PageServerLoad = async ({ fetch }) => {
+  let res;
 
-    let res;
+  try {
+    res = await fetch('/book-applets.json', {
+      cache: 'force-cache'
+    });
+  } catch (_) {
+    return {};
+  }
 
-    try {
-        res = await fetch('/book-applets.json', {
-            cache: 'force-cache'
-        });
-    } catch (_) {
-        return {};
-    }
+  if (!res.ok) {
+    return {};
+  }
 
-    if (!res.ok) {
-        return {};
-    }
+  const usage: AppletsUsageByBook = await res.json();
 
-    const usage: AppletsUsageByBook = await res.json();
-
-    return usage;
+  return usage;
 };
