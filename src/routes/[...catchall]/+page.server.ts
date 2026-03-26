4823ddd45579e2e6d8b628f282b2a1569eec6096
Bartosz Włodarczyk <61749397+bewuwy@users.noreply.github.com>
2026-03-26 16:33:09 +0100

    Fixed book applet usage check (#402)
    * moved book-applets to src

--- src/routes/[...catchall]/+page.server.ts
+++ src/routes/[...catchall]/+page.server.ts
@@ -1,34 +1,6 @@
 import type { PageServerLoad } from './$types';
+import { loadBookAppletsUsage } from '$lib/server/bookApplets';
 
-type AppletUsageEntry = {
-  url: string;
-  title: string;
-  name: string;
-  source_files: string[];
-};
-
-type BookAppletUsage = {
-  applets: AppletUsageEntry[];
-};
-
-type AppletsUsageByBook = Record<string, BookAppletUsage>;
-
-export const load: PageServerLoad = async ({ fetch }) => {
-  let res;
-
-  try {
-    res = await fetch('/book-applets.json', {
-      cache: 'force-cache'
-    });
-  } catch (_) {
-    return {};
-  }
-
-  if (!res.ok) {
-    return {};
-  }
-
-  const usage: AppletsUsageByBook = await res.json();
-
-  return usage;
+export const load: PageServerLoad = () => {
+  return loadBookAppletsUsage();
 };
