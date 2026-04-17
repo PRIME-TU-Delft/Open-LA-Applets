174e46482e624a92451a92e95db281f2af80f9b4
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2026-04-17 13:55:53 +0200

    Add inverse functions applet pages
    Add numerous new Svelte applet pages under src/routes/applet/calculus/inverse_functions. The changes introduce interactive Canvas2D/TemplateComponent-based visualizations for teaching inverse functions and reflections, including polling question pages (A–F), graphs (sine, cosine, tangent, cotangent), restricted trig graphs, and reflection/inverse examples for exponentials (base 2 and 1/2), linear, quadratic, square root, composite functions, and others. Each page sets up viewbox/camera, axis labels, legend items and AppletObjects (FunctionFragment/AsymptoteFragment) to render the examples.

---
+++ src/routes/applet/calculus/inverse_functions/graph_of_restricted_tangent_function/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  // For ease of creating the template applets
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  import {
+    AppletObject,
+    AsymptoteFragment,
+    FunctionFragment,
+    ObliqueAsymptoteFragment
+  } from '$lib/template/TemplateAppletObjects';
+  import TemplateComponent from '$lib/template/TemplateComponent.svelte';
+  import Canvas2D from '$lib/d3/Canvas2D.svelte';
+  import { PrimeColor } from '$lib/utils/PrimeColors';
+  import { Vector2 } from 'three';
+  import { ViewBox } from '$lib/d3/ViewBox';
+  import { getLegend } from '$lib/template/ObjectFormulas';
+  import { toLatexText } from '$lib/utils/FormatString';
+
+  let initialViewBox: ViewBox | undefined;
+  let cameraPosition: Vector2 | undefined;
+  let cameraZoom: number | undefined;
+  let xAxisLabel: string | undefined;
+  let yAxisLabel: string | undefined;
+
+  // ###############
+  // CAMERA SETTINGS
+  // ###############
+  // choose one or none of the options below - if both are specified, view box will be used
+
+  // (remove if unnecessary)
+  cameraPosition = new Vector2(3, 1);
+  cameraZoom = 1.5;
+
+  // (remove if unnecessary)
+  initialViewBox = new ViewBox(
+    new Vector2(-4, -4), // bottom-left
+    new Vector2(4, 4), // top-right
+    0.5 // margin
+  );
+
+  // ###########
+  // AXIS LABELS
+  // ###########
+
+  // (remove if unnecessary)
+  xAxisLabel = 'x';
+  yAxisLabel = 'y';
+
+  // ##############
+  // APPLET OBJECTS
+  // ##############
+  const appletObjects: AppletObject[] = [
+    new FunctionFragment('\\tan(x)', PrimeColor.blue, {
+      isDashed: false,
+      shape: 'square',
+      legendText: '\\tan(x)',
+      domain: { xMin: -Math.PI / 2, xMax: Math.PI / 2 }
+    })
+  ];
+</script>
+
+<Canvas2D
+  {initialViewBox}
+  {cameraPosition}
+  {cameraZoom}
+  legendItems={getLegend(appletObjects)}
+  labels={{ xLabel: xAxisLabel ?? undefined, yLabel: yAxisLabel ?? undefined }}
+>
+  <TemplateComponent objects={appletObjects} />
+</Canvas2D>
