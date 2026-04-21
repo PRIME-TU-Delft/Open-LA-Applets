2e3c697bf82f76b6a761ebdb3840e7b26567b86c
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2026-04-21 11:48:44 +0200

    Add two rational functions applet page
    Create a new Svelte page at src/routes/applet/calculus/limits_at_a_point/two_rational_functions/+page.svelte that implements a small applet demonstrating limits of two rational functions. Sets up Canvas2D with initial viewbox, camera position/zoom, and axis labels, and registers two FunctionFragment objects (f(x)=(x-1)/(x^2-1) and g(x)=x/(x^2-2x+1)) with styling, legend text and a gap on the first function. Imports TemplateComponent, Canvas2D, helper types and utilities and exposes the legend for the canvas.

---
+++ src/routes/applet/calculus/limits_at_a_point/two_rational_functions/+page.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  /* eslint-disable @typescript-eslint/no-unused-vars */ // For ease of creating the template applets
+  import {
+    AppletObject,
+    AsymptoteFragment,
+    FunctionFragment,
+    ObliqueAsymptoteFragment
+  } from '$lib/template/TemplateAppletObjects';
+  import TemplateComponent from '$lib/template/TemplateComponent.svelte';
+  import Canvas2D from '$lib/d3/Canvas2D.svelte';
+  import { PrimeColor } from '$lib/utils/PrimeColors';
+  import { Vector2 } from 'three';
+  import { ViewBox } from '$lib/d3/ViewBox';
+  import { getLegend } from '$lib/template/ObjectFormulas';
+  import { toLatexText } from '$lib/utils/FormatString';
+
+  let initialViewBox: ViewBox | undefined;
+  let cameraPosition: Vector2 | undefined;
+  let cameraZoom: number | undefined;
+  let xAxisLabel: string | undefined;
+  let yAxisLabel: string | undefined;
+
+  // ########################
+  // TUTORIAL / DOCUMENTATION
+  // ########################
+  // https://docs.openla.ewi.tudelft.nl/?path=/docs/tutorials-tutorial-template--docs
+  // on this page you can find documentation for the template objects and a tutorial on using them
+
+  // ###############
+  // CAMERA SETTINGS
+  // ###############
+  // choose one or none of the options below - if both are specified, view box will be used
+
+  // (remove if unnecessary)
+  cameraPosition = new Vector2(3, 1);
+  cameraZoom = 1.5;
+
+  // (remove if unnecessary)
+  initialViewBox = new ViewBox(
+    new Vector2(-3, -4), // bottom-left
+    new Vector2(5, 4), // top-right
+    0.5 // margin
+  );
+
+  // ###########
+  // AXIS LABELS
+  // ###########
+
+  // (remove if unnecessary)
+  xAxisLabel = 'x';
+  yAxisLabel = 'y';
+
+  // ##############
+  // APPLET OBJECTS
+  // ##############
+  const appletObjects: AppletObject[] = [
+    new FunctionFragment('\\frac{x-1}{x^2-1}', PrimeColor.blue, {
+      isDashed: false,
+      shape: 'circle',
+      legendText: 'f(x)=\\frac{x-1}{x^2-1}'
+    }).addGaps(new Vector2(1, 0.5)),
+    new FunctionFragment('\\frac{x}{x^2-2x+1}', PrimeColor.darkGreen, {
+      isDashed: false,
+      shape: 'square',
+      legendText: 'g(x)=\\frac{x}{x^2-2x+1}'
+    })
+  ];
+</script>
+
+<Canvas2D
+  {initialViewBox}
+  {cameraPosition}
+  {cameraZoom}
+  legendItems={getLegend(appletObjects)}
+  labels={{ xLabel: xAxisLabel ?? undefined, yLabel: yAxisLabel ?? undefined }}
+>
+  <TemplateComponent objects={appletObjects} />
+</Canvas2D>
