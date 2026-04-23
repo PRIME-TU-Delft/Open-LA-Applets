997dea97d636a8b76ff09c4f3ed682746f53b97d
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2026-04-23 18:41:00 +0200

    Add inverse-functions polling question applets
    Rename to non-capital

---
+++ src/routes/applet/calculus/inverse_functions/polling_question_B/+page.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  // For ease of creating the template applets
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  import {
+    AppletObject,
+    AsymptoteFragment,
+    FunctionFragment,
+    ObliqueAsymptoteFragment
+  } from '$lib/template/TemplateAppletObjects';
+  import TemplateComponent from '$lib/template/TemplateComponent.svelte';
+  import Canvas2D from '$lib/d3/Canvas2D.svelte';
+  import { PrimeColor } from '$lib/utils/PrimeColors';
+  import { Vector2 } from 'three';
+  import { ViewBox } from '$lib/d3/ViewBox';
+  import { getLegend } from '$lib/template/ObjectFormulas';
+  import { toLatexText } from '$lib/utils/FormatString';
+
+  let initialViewBox: ViewBox | undefined;
+  let cameraPosition: Vector2 | undefined;
+  let cameraZoom: number | undefined;
+  let xAxisLabel: string | undefined;
+  let yAxisLabel: string | undefined;
+
+  // ###############
+  // CAMERA SETTINGS
+  // ###############
+  // choose one or none of the options below - if both are specified, view box will be used
+
+  // (remove if unnecessary)
+  cameraPosition = new Vector2(3, 1);
+  cameraZoom = 1.5;
+
+  // (remove if unnecessary)
+  initialViewBox = new ViewBox(
+    new Vector2(-4, -4), // bottom-left
+    new Vector2(4, 4), // top-right
+    0.5 // margin
+  );
+
+  // ###########
+  // AXIS LABELS
+  // ###########
+
+  // (remove if unnecessary)
+  xAxisLabel = 'x';
+  yAxisLabel = 'y';
+
+  // ##############
+  // APPLET OBJECTS
+  // ##############
+  const appletObjects: AppletObject[] = [
+    new FunctionFragment('2x', PrimeColor.blue, {
+      isDashed: false,
+      shape: 'square',
+      legendText: 'f(x)=2x'
+    }),
+    new FunctionFragment('x/2', PrimeColor.darkGreen, {
+      isDashed: true,
+      shape: 'square',
+      legendText: '\\text{Reflection of }f\\text{ in }y=x'
+    }),
+    new FunctionFragment('x', PrimeColor.raspberry, {
+      isDashed: true,
+      shape: 'triangle',
+      legendText: 'y=x'
+    })
+  ];
+</script>
+
+<Canvas2D
+  {initialViewBox}
+  {cameraPosition}
+  {cameraZoom}
+  legendItems={getLegend(appletObjects)}
+  labels={{ xLabel: xAxisLabel ?? undefined, yLabel: yAxisLabel ?? undefined }}
+  position="top-left"
+>
+  <TemplateComponent objects={appletObjects} />
+</Canvas2D>
