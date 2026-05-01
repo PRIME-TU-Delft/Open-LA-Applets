2c71c406af08d15800e36620a95f746bc10e0179
Dennis den Ouden-van der Horst <49307154+douden@users.noreply.github.com>
2026-05-01 14:01:03 +0200

    Add indefinite integral applets; bump svelte-check (#425)
    Add two new template applets for calculus/indefinite_integrals (a_function and several_functions) that demonstrate FunctionFragment usage (single function and multiple toggled functions) and integrate with Canvas2D, TemplateComponent, and Controls. Minor ESLint/comment cleanup in existing template pages. Bump svelte-check to 4.4.7 in package.json and update pnpm-lock.yaml with corresponding dependency resolution changes (svelte-check, svelte2tsx, postcss, devalue, nanoid, ip-address, b4a, bare-os, etc.).

--- src/routes/applet/calculus/indefinite_integrals/a_function/+page.svelte
+++ src/routes/applet/calculus/indefinite_integrals/a_function/+page.svelte
@@ -2,27 +2,35 @@
   /* eslint-disable @typescript-eslint/no-unused-vars */
   // For ease of creating the template applets
   import {
+    Angle,
     AppletObject,
     AsymptoteFragment,
+    Circle,
     FunctionFragment,
-    ObliqueAsymptoteFragment
+    ImplicitFunctionFragment,
+    LineFragment,
+    ObliqueAsymptoteFragment,
+    ParameterizedFunctionFragment,
+    Point,
+    Polygon,
+    Text
   } from '$lib/template/TemplateAppletObjects';
   import TemplateComponent from '$lib/template/TemplateComponent.svelte';
   import Canvas2D from '$lib/d3/Canvas2D.svelte';
-  import Axis from '$lib/d3/Axis.svelte';
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Vector2 } from 'three';
   import { ViewBox } from '$lib/d3/ViewBox';
   import { getLegend } from '$lib/template/ObjectFormulas';
   import { toLatexText } from '$lib/utils/FormatString';
-  import Latex2D from '$lib/d3/Latex2D.svelte';
-  import Latex from '$lib/components/Latex.svelte';
+  import type { AxisProps } from '$lib/d3/Axis.svelte';
+  import Legend from '$lib/stories/Legend.stories.svelte';
 
   let initialViewBox: ViewBox | undefined;
   let cameraPosition: Vector2 | undefined;
   let cameraZoom: number | undefined;
   let xAxisLabel: string | undefined;
   let yAxisLabel: string | undefined;
+  let axis: AxisProps | undefined;
 
   // ########################
   // TUTORIAL / DOCUMENTATION
@@ -37,14 +45,25 @@
 
   // (remove if unnecessary)
   cameraPosition = new Vector2(3, 1);
-  cameraZoom = 1.5;
+  cameraZoom = 1.25;
+
+  // ####
+  // AXIS
+  // ####
+  // here are the default settings for axis, you can change them
 
   // (remove if unnecessary)
-  initialViewBox = new ViewBox(
-    new Vector2(-5, -4), // bottom-left
-    new Vector2(5, 6), // top-right
-    0.5 // margin
-  );
+  axis = {
+    showOrigin: true,
+    showAxisNumbersX: true,
+    showAxisNumbersY: true,
+    logarithmicX: false,
+    logarithmicY: false,
+    scaleX: 1,
+    scaleY: 1,
+    skipX: 0,
+    skipY: 0
+  };
 
   // ###########
   // AXIS LABELS
@@ -57,45 +76,25 @@
   // ##############
   // APPLET OBJECTS
   // ##############
+  let a = -1;
+  let b = 2;
+  let c = 4;
+  let scale = 0.2;
+  let f = (x: number) => scale * (x - a) * (x - b) * (x - c);
   const appletObjects: AppletObject[] = [
-    new FunctionFragment('|x|', PrimeColor.blue, {
-      legendText: '|x|',
-      integral: {
-        xLeft: (-Math.PI * 7) / 8,
-        xRight: (Math.PI * 7) / 8,
-        legendText: '\\int_{-a}^{a} |x| dx',
-        isDashed: false,
-        color: PrimeColor.darkGreen,
-        shape: 'square'
-      }
+    new FunctionFragment(f, PrimeColor.blue, {
+      legendText: `y=f(x)`
     })
   ];
 </script>
 
 <Canvas2D
-  {initialViewBox}
+  // {initialViewBox}
   {cameraPosition}
   {cameraZoom}
   legendItems={getLegend(appletObjects)}
   labels={{ xLabel: xAxisLabel ?? undefined, yLabel: yAxisLabel ?? undefined }}
-  legendFormulaPosition="top-left"
-  axis={null}
+  {axis}
 >
   <TemplateComponent objects={appletObjects} />
-  <Axis
-    skipX={100}
-    skipY={100}
-    showAxisNumbersX={false}
-    showAxisNumbersY={false}
-    additionalTicksX={[(-7 / 8) * Math.PI, (7 / 8) * Math.PI]}
-    additionalTicksY={[(7 / 8) * Math.PI]}
-  />
-  <Latex2D latex="a" position={new Vector2((7 / 8) * Math.PI, -0.15)} alignX="center" />
-  <Latex2D latex="-a" position={new Vector2((-7 / 8) * Math.PI, -0.15)} alignX="center" />
-  <Latex2D
-    latex="a"
-    position={new Vector2(-0.15, (7 / 8) * Math.PI)}
-    alignY="center"
-    alignX="right"
-  />
 </Canvas2D>
