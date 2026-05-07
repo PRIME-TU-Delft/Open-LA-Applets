8a440216d3d91aa564a7a37a0ab50db19efbb25e
Bartek Włodarczyk <bewuw@proton.me>
2026-05-07 21:53:52 +0200

    feat: added PTS front page

---
+++ src/routes/applet/pts/clt/clt/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Canvas2D from '$lib/d3/Canvas2D.svelte';
+  import Vector2D from '$lib/d3/Vector2D.svelte';
+</script>
+
+<Canvas2D>
+  <Vector2D />
+</Canvas2D>
--- src/routes/pts/[...catchall]/+page.svelte
+++ src/routes/pts/[...catchall]/+page.svelte
@@ -4,40 +4,23 @@
   import NavBar from '$lib/components/frontpage/NavBar.svelte';
   import { resolve } from '$app/paths';
   import Credits from '$lib/components/frontpage/Credits.svelte';
-  import type { PageProps } from './$types';
 
-  const modules = import.meta.glob('/src/routes/applet/**/+page.svelte');
+  const modules = import.meta.glob('/src/routes/applet/pts/**/+page.svelte');
 
-  const fileUrls = Object.keys(modules)
-    .map((rawUrl) =>
-      // Remove head of path and extension
-      rawUrl.replace('/src/routes/applet/', '').replace('/+page.svelte', '')
-    )
-    .filter(
-      (s) => s !== '[...applet]/static' && !s.startsWith('calculus/') && !s.startsWith('other/') && !s.startsWith('pts/')
-    );
-
-  let { data }: PageProps = $props();
+  const fileUrls = Object.keys(modules).map((rawUrl) =>
+    // Remove head of path and extension
+    rawUrl.replace('/src/routes/applet/pts/', '').replace('/+page.svelte', '')
+  );
 </script>
 
-<NavBar
-  bookTitle="Open LA book"
-  bookURL="https://interactivetextbooks.tudelft.nl/linear-algebra/"
-  {fileUrls}
-/>
+<NavBar bookTitle="Probability Theory and Statistics" {fileUrls} directory="pts/" />
 
 <div class="mx-auto my-32 max-w-4xl rounded-xl bg-blue-50 p-10">
   <div class="prose w-full max-w-full">
-    <h1>Interactive Linear Algebra applets</h1>
-    These applets were created for the
-    <a
-      class="inline-flex items-center gap-1 hover:underline"
-      href="https://interactivetextbooks.tudelft.nl/linear-algebra/"
-    >
-      TU Delft Open Linear Algebra book <ExternalLink class="h-4 w-4" /></a
-    >. Abstract linear algebra concepts are visualized in an interactive way to help students
-    understand the material better. Using 2D and 3D graphics, the applets provide a hands-on
-    experience to explore the concepts.
+    <h1>Interactive Probability Theory and Statistics applets</h1>
+    These applets were created for the TU Delft Probability Theory and Statistics course. Abstract concepts
+    are visualized in an interactive way to help students understand the material better. Using 2D and
+    3D graphics, the applets provide a hands-on experience to explore the concepts.
 
     <p>
       The applets are sorted by the paragraph that they are associated with in the book. Feel free
@@ -46,9 +29,9 @@
 
     <blockquote class="border-green-400/50 bg-green-400/10 py-2 text-green-800">
       <span class="font-bold">Hint:</span>
-      Looking for Calculus applets? Go to
-      <a class="inline-flex items-center gap-1 hover:underline" href={resolve('/calculus')}
-        >/calculus <ExternalLink class="h-4 w-4" /></a
+      Looking for Linear Algebra applets? Go to the
+      <a class="inline-flex items-center gap-1 hover:underline" href={resolve('/')}
+        >main page <ExternalLink class="h-4 w-4" /></a
       >
     </blockquote>
 
@@ -71,12 +54,9 @@
     </blockquote>
   </div>
 
-  <FolderList {fileUrls} appletUsageInBook={data['Linear-Algebra']} />
+  <FolderList {fileUrls} directory="pts/" />
 
-  <Credits
-    bookURL="https://interactivetextbooks.tudelft.nl/linear-algebra/"
-    bookName="TU Delft Open Linear Algebra book"
-  />
+  <Credits />
 
   <div class="rounded bg-slate-300 p-10">
     <img class="h-20 w-full object-contain" alt="prime-tudelft" src="/logo-black.png" />
