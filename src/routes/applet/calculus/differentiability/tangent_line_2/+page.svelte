f623c99e059eb68403a8c5700b3a592afdca8a9a
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2026-07-01 09:08:10 +0200

    Add tangent_line_2 applet and fix viewbox
    Add new tangent line applet for differentiability section showing f(x)=x² with a draggable point and its tangent line. Also fix the initialViewBox coordinates in introduction_differentiability.

--- src/routes/applet/calculus/differentiability/tangent_line_2/+page.svelte
+++ src/routes/applet/calculus/differentiability/tangent_line_2/+page.svelte
@@ -6,8 +6,10 @@
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Vector2 } from 'three';
   import { ViewBox } from '$lib/d3/ViewBox';
-  import { getLegend } from '$lib/template/ObjectFormulas';
   import type { AxisProps } from '$lib/d3/Axis.svelte';
+  import { Draggable } from '$lib/controls/Draggables.svelte';
+  import { LegendItem } from '$lib/utils/Legend';
+  import InfiniteLine2D from '$lib/d3/InfiniteLine2D.svelte';
 
   let initialViewBox: ViewBox | undefined;
   let cameraPosition: Vector2 | undefined;
@@ -31,13 +33,6 @@
   cameraPosition = new Vector2(3, 1);
   cameraZoom = 1.5;
 
-  // (remove if unnecessary)
-  initialViewBox = new ViewBox(
-    new Vector2(-100, -200), // bottom-left
-    new Vector2(1500, 1000), // top-right
-    0.5 // margin
-  );
-
   // ####
   // AXIS
   // ####
@@ -50,8 +45,8 @@
     showAxisNumbersY: true,
     logarithmicX: false,
     logarithmicY: false,
-    skipX: 1,
-    skipY: 1
+    skipX: 0,
+    skipY: 0
   };
 
   // #####
@@ -60,48 +55,80 @@
   // All child components (functions, points, lines, etc.) will auto-scale accordingly.
   // Example: scaleX={2} means 1 unit in world space = 2 display units on the x-axis.
   // Formulas and positions should be written in display (mathematical) space.
-  let scaleX = 1 / 100;
-  let scaleY = 1 / 100;
+  let scaleX = 1;
+  let scaleY = 1;
+
+  // (remove if unnecessary)
+  initialViewBox = new ViewBox(
+    new Vector2(-3, -4), // bottom-left
+    new Vector2(4, 6), // top-right
+    0.5 // margin
+  );
 
   // ###########
   // AXIS LABELS
   // ###########
 
   // (remove if unnecessary)
-  xAxisLabel = 't';
-  yAxisLabel = 'x';
+  xAxisLabel = 'x';
+  yAxisLabel = 'y';
 
   // ##############
   // APPLET OBJECTS
   // ##############
-  const appletObjects: AppletObject[] = [
-    new FunctionFragment((x: number) => (3 / 2) * x, PrimeColor.blue, {
-      domain: { xMin: 0, xMax: 400 },
-      legendText: 's(t)=\\frac{3}{2}t',
-      width: 0.1
-    }).addIncludedPoints([new Vector2(0, 0), new Vector2(400, 600)], undefined, 0.15),
-    new FunctionFragment(
-      (x: number) => 300 * Math.sin((3 * Math.PI * x) / 1200 - Math.PI / 2) + 300,
-      PrimeColor.raspberry,
-      {
-        domain: { xMin: 0, xMax: 1200 },
-        shape: 'square',
-        legendText: 'x(t)=300\\sin\\left(\\frac{3\\pi t}{1200}-\\frac{\\pi}{2}\\right)+300',
-        width: 0.1
-      }
-    ).addIncludedPoints([new Vector2(0, 0), new Vector2(1200, 600)], undefined, 0.15)
+  function baseFunction(x: number): number {
+    return Math.pow(x, 2);
+  }
+  function baseDerivative(x: number): number {
+    return 2 * x;
+  }
+  function snapToFunc(position: Vector2): Vector2 {
+    const x = Number(position.x.toFixed(2)); // in increments of 0.01;
+    const y = baseFunction(x);
+    return new Vector2(x, y);
+  }
+  const initX = 1;
+  const draggablePoint = [
+    new Draggable(
+      new Vector2(initX, baseFunction(initX)),
+      PrimeColor.orange,
+      undefined,
+      snapToFunc,
+      undefined,
+      'right',
+      0.15
+    )
   ];
+  const appletObjects: AppletObject[] = [
+    new FunctionFragment(baseFunction, PrimeColor.blue, { width: 0.08 })
+  ];
+  function legendA() {
+    const point = draggablePoint[0].position;
+    return `(a,f(a))\\approx(${point.x.toFixed(2).replace(/\.00$/, '')}, ${point.y.toFixed(2).replace(/\.00$/, '')})`;
+  }
+  const legendItems = $derived([
+    new LegendItem('f(x)=x^2', PrimeColor.blue),
+    new LegendItem(legendA(), PrimeColor.orange)
+  ]);
 </script>
 
 <Canvas2D
+  draggables={draggablePoint}
   {initialViewBox}
   {cameraPosition}
   {cameraZoom}
-  legendItems={getLegend(appletObjects)}
+  {legendItems}
   labels={{ xLabel: xAxisLabel ?? undefined, yLabel: yAxisLabel ?? undefined }}
   {axis}
   {scaleX}
   {scaleY}
+  legendFormulaPosition="top-left"
 >
   <TemplateComponent objects={appletObjects} />
+  <InfiniteLine2D
+    origin={draggablePoint[0].position}
+    direction={new Vector2(1, baseDerivative(draggablePoint[0].position.x))}
+    color={PrimeColor.darkGreen}
+    width={0.08}
+  />
 </Canvas2D>
