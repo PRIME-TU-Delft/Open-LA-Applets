fe338d6da867e2522afdf4b17001178205044ab0
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2026-07-15 12:59:24 +0200

    Add roots-of-cubic applet page
    Create a new calculus applet page for "roots_of_a_cubic_function" using the template Canvas2D setup. It plots the cubic f(x)=x^3+3x^2+6x+3 with a dashed full curve, highlights the segment on [-1, 0] with included endpoints, and marks the computed real root where f(x)=0.

---
+++ src/routes/applet/calculus/graphs_and_extreme_values/roots_of_a_cubic_function/+page.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+  // For ease of creating the template applets
+  import { AppletObject, FunctionFragment, Point } from '$lib/template/TemplateAppletObjects';
+  import TemplateComponent from '$lib/template/TemplateComponent.svelte';
+  import Canvas2D from '$lib/d3/Canvas2D.svelte';
+  import { PrimeColor } from '$lib/utils/PrimeColors';
+  import { Vector2 } from 'three';
+  import { getLegend } from '$lib/template/ObjectFormulas';
+  import type { AxisProps } from '$lib/d3/Axis.svelte';
+
+  let cameraPosition: Vector2 | undefined;
+  let cameraZoom: number | undefined;
+  let xAxisLabel: string | undefined;
+  let yAxisLabel: string | undefined;
+  let axis: AxisProps | undefined;
+
+  // ########################
+  // TUTORIAL / DOCUMENTATION
+  // ########################
+  // https://docs.openla.ewi.tudelft.nl/?path=/docs/tutorials-tutorial-template--docs
+  // on this page you can find documentation for the template objects and a tutorial on using them
+
+  // ###############
+  // CAMERA SETTINGS
+  // ###############
+  // choose one or none of the options below - if both are specified, view box will be used
+
+  // (remove if unnecessary)
+  cameraPosition = new Vector2(1, 1);
+  cameraZoom = 1.5;
+
+  // ####
+  // AXIS
+  // ####
+  // here are the default settings for axis, you can change them
+
+  // (remove if unnecessary)
+  axis = {
+    showOrigin: true,
+    showAxisNumbersX: true,
+    showAxisNumbersY: true,
+    logarithmicX: false,
+    logarithmicY: false,
+    skipX: 0,
+    skipY: 0
+  };
+
+  // #####
+  // SCALE
+  // #####
+  // All child components (functions, points, lines, etc.) will auto-scale accordingly.
+  // Example: scaleX={2} means 1 unit in world space = 2 display units on the x-axis.
+  // Formulas and positions should be written in display (mathematical) space.
+  let scaleX = 1;
+  let scaleY = 1;
+
+  // ###########
+  // AXIS LABELS
+  // ###########
+
+  // (remove if unnecessary)
+  xAxisLabel = 'x';
+  yAxisLabel = 'y';
+
+  // ##############
+  // APPLET OBJECTS
+  // ##############
+  const Function = (x: number) => x ** 3 + 3 * x ** 2 + 6 * x + 3;
+  const Zero =
+    -1 - Math.pow(2 / (1 + Math.sqrt(5)), 1 / 3) + Math.pow((1 / 2) * (1 + Math.sqrt(5)), 1 / 3);
+  const appletObjects: AppletObject[] = [
+    new FunctionFragment(Function, PrimeColor.blue, {
+      isDashed: true,
+      width: 0.06
+    }),
+    new FunctionFragment(Function, PrimeColor.blue, {
+      legendText: 'f(x)=x^3+3x^2+6x+3',
+      domain: { xMin: -1, xMax: 0 },
+      width: 0.06
+    }).addIncludedPoints(
+      [new Vector2(-1, Function(-1)), new Vector2(0, Function(0))],
+      undefined,
+      0.08
+    ),
+    new Point(new Vector2(Zero, 0), PrimeColor.raspberry, { legendText: 'f(x)=0', radius: 0.08 })
+  ];
+</script>
+
+<Canvas2D
+  {cameraPosition}
+  {cameraZoom}
+  legendItems={getLegend(appletObjects)}
+  labels={{ xLabel: xAxisLabel ?? undefined, yLabel: yAxisLabel ?? undefined }}
+  {axis}
+  {scaleX}
+  {scaleY}
+>
+  <TemplateComponent objects={appletObjects} />
+</Canvas2D>
