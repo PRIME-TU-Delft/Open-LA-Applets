0cb23d21f9a76f23287c5ef57f7c9504a8b59b54
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2026-07-16 16:09:38 +0200

    Add FTC part 1 proof and dual visual applets
    Adds two new calculus applet routes for Fundamental Theorem of Calculus part 1. The first is a proof-oriented static plot showing a function fragment with the integral over [x, x+h] and labeled axis markers. The second is a two-panel interactive visualization: one panel shows the shaded integral of f(t)=2e^{-t^2/4} from 0 to x, and the other shows the corresponding accumulation function F(x) with a local linear approximation, using synchronized draggable x-controls.

---
+++ src/routes/applet/calculus/fundamental_theorem_of_calculus/proof_of_part_1/+page.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  /* eslint-disable @typescript-eslint/no-unused-vars */ // For ease of creating the template applets
+  import { AppletObject, FunctionFragment } from '$lib/template/TemplateAppletObjects';
+  import TemplateComponent from '$lib/template/TemplateComponent.svelte';
+  import Canvas2D from '$lib/d3/Canvas2D.svelte';
+  import Axis from '$lib/d3/Axis.svelte';
+  import { PrimeColor } from '$lib/utils/PrimeColors';
+  import { Vector2 } from 'three';
+  import { ViewBox } from '$lib/d3/ViewBox';
+  import { getLegend } from '$lib/template/ObjectFormulas';
+  import { toLatexText } from '$lib/utils/FormatString';
+  import Latex2D from '$lib/d3/Latex2D.svelte';
+
+  let initialViewBox: ViewBox | undefined;
+  let cameraPosition: Vector2 | undefined;
+  let cameraZoom: number | undefined;
+  let xAxisLabel: string | undefined;
+  let yAxisLabel: string | undefined;
+
+  // ########################
+  // TUTORIAL / DOCUMENTATION
+  // ########################
+  // https://docs.openla.ewi.tudelft.nl/?path=/docs/tutorials-tutorial-template--docs
+  // on this page you can find documentation for the template objects and a tutorial on using them
+
+  // ###############
+  // CAMERA SETTINGS
+  // ###############
+  // choose one or none of the options below - if both are specified, view box will be used
+
+  // (remove if unnecessary)
+  cameraPosition = new Vector2(3, 1);
+  cameraZoom = 1.5;
+
+  // (remove if unnecessary)
+  initialViewBox = new ViewBox(
+    new Vector2(-1, -1), // bottom-left
+    new Vector2(6, 5), // top-right
+    0.5 // margin
+  );
+
+  // ###########
+  // AXIS LABELS
+  // ###########
+
+  // (remove if unnecessary)
+  xAxisLabel = 't';
+  yAxisLabel = 'y';
+
+  // ##############
+  // APPLET OBJECTS
+  // ##############
+  const x = 3;
+  const h = 0.5;
+  const appletObjects: AppletObject[] = [
+    new FunctionFragment(
+      '-\\frac{2}{7}x^{3} + 3x^{2} - \\frac{135}{14}x + \\frac{153}{14}',
+      PrimeColor.blue,
+      {
+        legendText: 'f(t)',
+        integral: {
+          xLeft: x,
+          xRight: x + h,
+          legendText: '\\int_{x}^{x+h} f(t)\\,dt',
+          isDashed: false,
+          color: PrimeColor.darkGreen,
+          shape: 'square'
+        }
+      }
+    )
+  ];
+</script>
+
+<Canvas2D
+  {initialViewBox}
+  {cameraPosition}
+  {cameraZoom}
+  legendItems={getLegend(appletObjects)}
+  labels={{ xLabel: xAxisLabel ?? undefined, yLabel: yAxisLabel ?? undefined }}
+  axis={null}
+>
+  <TemplateComponent objects={appletObjects} />
+  <Axis
+    showAxisNumbersX={false}
+    showAxisNumbersY={false}
+    showGridLinesX={true}
+    showGridLinesY={false}
+    skipX={-1}
+    skipY={-1}
+    additionalTicksX={[1, 5, x, x + h]}
+  />
+  <Latex2D latex="a" position={new Vector2(1, -0.15)} alignX="right" />
+  <Latex2D latex="b" position={new Vector2(5, -0.15)} alignX="left" />
+  <Latex2D latex="x" position={new Vector2(x, -0.15)} alignX="right" />
+  <Latex2D latex="x+h" position={new Vector2(x + h, -0.15)} alignX="left" />
+</Canvas2D>
---
+++ src/routes/applet/calculus/fundamental_theorem_of_calculus/visualisation_1_of_part_1/+page.svelte
@@ -0,0 +1,214 @@
+<script lang="ts">
+  // For ease of creating the template applets
+  import { AppletObject, FunctionFragment } from '$lib/template/TemplateAppletObjects';
+  import { PrimeColor } from '$lib/utils/PrimeColors';
+  import { Vector2 } from 'three';
+  import { ViewBox } from '$lib/d3/ViewBox';
+  import { getLegend } from '$lib/template/ObjectFormulas';
+  import { toLatexText } from '$lib/utils/FormatString';
+  import type { AxisProps } from '$lib/d3/Axis.svelte';
+  import { Draggable } from '$lib/controls/Draggables.svelte';
+  import ExplicitFunction2D from '$lib/d3/ExplicitFunction2D.svelte';
+  import Latex2D from '$lib/d3/Latex2D.svelte';
+  import CanvasGrid from '$lib/common/CanvasGrid.svelte';
+  import GridCanvas2D from '$lib/common/GridCanvas2D.svelte';
+  import Point2D from '$lib/d3/Point2D.svelte';
+
+  let initialViewBox: ViewBox | undefined;
+  let cameraPosition: Vector2 | undefined;
+  let cameraZoom: number | undefined;
+  let xAxisLabel: string | undefined;
+  let yAxisLabel: string | undefined;
+  let axis: AxisProps | undefined;
+
+  // ########################
+  // TUTORIAL / DOCUMENTATION
+  // ########################
+  // https://docs.openla.ewi.tudelft.nl/?path=/docs/tutorials-tutorial-template--docs
+  // on this page you can find documentation for the template objects and a tutorial on using them
+
+  // ###############
+  // CAMERA SETTINGS
+  // ###############
+  // choose one or none of the options below - if both are specified, view box will be used
+
+  // (remove if unnecessary)
+  cameraPosition = new Vector2(2, 2);
+  cameraZoom = 2;
+
+  // ####
+  // AXIS
+  // ####
+  // here are the default settings for axis, you can change them
+
+  // (remove if unnecessary)
+  axis = {
+    showOrigin: true,
+    showAxisNumbersX: true,
+    showAxisNumbersY: true,
+    logarithmicX: false,
+    logarithmicY: false,
+    skipX: 0,
+    skipY: 0
+  };
+
+  // #####
+  // SCALE
+  // #####
+  // All child components (functions, points, lines, etc.) will auto-scale accordingly.
+  // Example: scaleX={2} means 1 unit in world space = 2 display units on the x-axis.
+  // Formulas and positions should be written in display (mathematical) space.
+  let scaleX = 1;
+  let scaleY = 1;
+
+  // ###########
+  // AXIS LABELS
+  // ###########
+
+  // (remove if unnecessary)
+  xAxisLabel = 't';
+  yAxisLabel = 'y';
+
+  // ##############
+  // APPLET OBJECTS
+  // ##############
+  // 2. Pure JS implementation (Abramowitz and Stegun approximation)
+  function calculateErf(x: number): number {
+    const sign = x >= 0 ? 1 : -1;
+    const absX = Math.abs(x);
+
+    // Constants
+    const p = 0.3275911;
+    const a1 = 0.254829592;
+    const a2 = -0.284496736;
+    const a3 = 1.421413741;
+    const a4 = -1.453152027;
+    const a5 = 1.061405429;
+
+    const t = 1.0 / (1.0 + p * absX);
+    const y = 1.0 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.exp(-absX * absX);
+
+    return sign * y;
+  }
+  const Integrand = (x: number) => 2 * Math.exp(-1 * (-0.5 * x) ** 2);
+  const Integral = (x: number) => 2 * Math.sqrt(Math.PI) * calculateErf(0.5 * x);
+  const appletObjects: AppletObject[] = [
+    new FunctionFragment(Integrand, PrimeColor.raspberry, {
+      legendText: 'f(t)=2e^{-\\frac{1}{4}t^2}',
+      domain: { xMin: 0 },
+      width: 0.06,
+      integral: {
+        xLeft: 0,
+        xRight: 0,
+        legendText: 'F(x)=\\displaystyle\\int_0^x2e^{-\\frac{1}{4}t^2}\\,dt',
+        color: PrimeColor.darkGreen
+      }
+    })
+  ];
+  function Snap(position: Vector2): Vector2 {
+    const x = Math.max(0, Number(position.x.toFixed(2))); // in increments of 0.01;
+    draggablePoint[1].value = new Vector2(x, 0);
+    return new Vector2(x, 0);
+  }
+  function Snap2(position: Vector2): Vector2 {
+    const x = Math.max(0, Number(position.x.toFixed(2))); // in increments of 0.01;
+    draggablePoint[0].value = new Vector2(x, 0);
+    return new Vector2(x, 0);
+  }
+  const initX = 1;
+  const draggablePoint = [
+    new Draggable(
+      new Vector2(initX, 0),
+      PrimeColor.orange,
+      undefined,
+      Snap,
+      undefined,
+      undefined,
+      0.14
+    ),
+    new Draggable(
+      new Vector2(initX, 0),
+      PrimeColor.orange,
+      undefined,
+      Snap2,
+      undefined,
+      undefined,
+      0.14
+    )
+  ];
+</script>
+
+<CanvasGrid
+  rows={1}
+  columns={2}
+  legendItems={[...getLegend(appletObjects)]}
+  legendFormulaPosition="top-left"
+>
+  <GridCanvas2D
+    draggables={[draggablePoint[0]]}
+    {initialViewBox}
+    {cameraPosition}
+    {cameraZoom}
+    labels={{ xLabel: xAxisLabel ?? undefined, yLabel: yAxisLabel ?? undefined }}
+    {axis}
+    {scaleX}
+    {scaleY}
+  >
+    <ExplicitFunction2D
+      func={Integrand}
+      xMin={0}
+      color={PrimeColor.raspberry}
+      width={0.06}
+      integral={{
+        xLeft: 0,
+        xRight: draggablePoint[0].position.x,
+        fillStyle: 'full',
+        color: PrimeColor.darkGreen
+      }}
+    />
+    <Point2D
+      position={new Vector2(draggablePoint[0].position.x, Integrand(draggablePoint[0].position.x))}
+      color={PrimeColor.purple}
+    />
+    <Latex2D
+      latex={toLatexText(`$x$`)}
+      color={PrimeColor.orange}
+      position={new Vector2(draggablePoint[0].position.x, -0.3)}
+      alignX="center"
+      alignY="top"
+    />
+  </GridCanvas2D>
+
+  <GridCanvas2D
+    draggables={[draggablePoint[1]]}
+    {initialViewBox}
+    {cameraPosition}
+    {cameraZoom}
+    labels={{ xLabel: 'x', yLabel: yAxisLabel ?? undefined }}
+    {axis}
+    {scaleX}
+    {scaleY}
+  >
+    <ExplicitFunction2D func={Integral} xMin={0} color={PrimeColor.darkGreen} width={0.06} />
+    <Latex2D
+      latex={toLatexText(`$x$`)}
+      color={PrimeColor.orange}
+      position={new Vector2(draggablePoint[0].position.x, -0.3)}
+      alignX="center"
+      alignY="top"
+    />
+    <ExplicitFunction2D
+      func={(x: number) =>
+        Integral(draggablePoint[0].position.x) +
+        Integrand(draggablePoint[0].position.x) * (x - draggablePoint[0].position.x)}
+      color={PrimeColor.raspberry}
+      width={0.06}
+      xMin={draggablePoint[0].position.x - 0.5}
+      xMax={draggablePoint[0].position.x + 0.5}
+    />
+    <Point2D
+      position={new Vector2(draggablePoint[0].position.x, Integral(draggablePoint[0].position.x))}
+      color={PrimeColor.purple}
+    />
+  </GridCanvas2D>
+</CanvasGrid>
