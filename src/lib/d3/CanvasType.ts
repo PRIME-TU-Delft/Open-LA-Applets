7298ca1b7906e62ce59ba9d471c7aca250917df3
Stella Schultz <60155656+smarlene@users.noreply.github.com>
2026-08-26 09:42:50 +0200

    Legend Position Options (#519)
    * refactor action buttons and formula to have separate legend formula panel, adjust scene to allow multiple placement options, add placement options for canvas3d
    
    * update styling for split formulas and top-left when no title present
    
    * update legendpositionformula in canvasgrid, adjust top-left placement, use single div for top-left and top-right handling
    
    * add storybook story for alternative legend positions
    
    * remove dead code from scene and legendformulapanel
    
    ---------
    
    Co-authored-by: Stella Marlene Schultz <s.m.schultz@student.tudelft.nl>

--- src/lib/d3/CanvasType.ts
+++ src/lib/d3/CanvasType.ts
@@ -2,11 +2,13 @@ import type { SceneProps } from '$lib/components/Scene.svelte';
 import type { Camera3DProps } from '$lib/threlte/CanvasType';
 import type { Snippet } from 'svelte';
 import { type Canvas2DProps } from './CanvasD3.svelte';
+import type { LegendFormulaAnchor } from '$lib/components/LegendFormulaPanel.svelte';
 
 export type CanvasProps = SceneProps &
   Omit<Canvas2DProps, 'children' | 'width'> & {
     splitCanvas2DProps?: Omit<Canvas2DProps, 'children' | 'width' | 'height' | 'isSplit'>;
     splitCanvas3DProps?: Camera3DProps; // Not implemented yet
+    legendFormulaPosition?: LegendFormulaAnchor;
     children: Snippet;
     splitCanvas2DChildren?: Snippet;
     splitCanvas3DChildren?: Snippet;
