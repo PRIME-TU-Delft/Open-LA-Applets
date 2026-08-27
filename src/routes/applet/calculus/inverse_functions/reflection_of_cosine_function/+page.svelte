7e4e8bf9f13df31b48818694898d964284a1b5df
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2026-08-27 11:06:45 +0200

    formatting and linting

--- src/routes/applet/calculus/inverse_functions/reflection_of_cosine_function/+page.svelte
+++ src/routes/applet/calculus/inverse_functions/reflection_of_cosine_function/+page.svelte
@@ -1,23 +1,14 @@
 <script lang="ts">
   // For ease of creating the template applets
-  /* eslint-disable @typescript-eslint/no-unused-vars */
-  import {
-    AppletObject,
-    AsymptoteFragment,
-    FunctionFragment,
-    ObliqueAsymptoteFragment
-  } from '$lib/template/TemplateAppletObjects';
+  import { AppletObject, FunctionFragment } from '$lib/template/TemplateAppletObjects';
   import TemplateComponent from '$lib/template/TemplateComponent.svelte';
   import Canvas2D from '$lib/d3/Canvas2D.svelte';
   import { PrimeColor } from '$lib/utils/PrimeColors';
   import { Vector2 } from 'three';
   import { ViewBox } from '$lib/d3/ViewBox';
   import { getLegend } from '$lib/template/ObjectFormulas';
-  import { toLatexText } from '$lib/utils/FormatString';
 
   let initialViewBox: ViewBox | undefined;
-  let cameraPosition: Vector2 | undefined;
-  let cameraZoom: number | undefined;
   let xAxisLabel: string | undefined;
   let yAxisLabel: string | undefined;
 
@@ -25,11 +16,6 @@
   // CAMERA SETTINGS
   // ###############
   // choose one or none of the options below - if both are specified, view box will be used
-
-  // (remove if unnecessary)
-  cameraPosition = new Vector2(3, 1);
-  cameraZoom = 1.5;
-
   // (remove if unnecessary)
   initialViewBox = new ViewBox(
     new Vector2(-4, -4), // bottom-left
@@ -70,8 +56,6 @@
 
 <Canvas2D
   {initialViewBox}
-  {cameraPosition}
-  {cameraZoom}
   legendItems={getLegend(appletObjects)}
   labels={{ xLabel: xAxisLabel ?? undefined, yLabel: yAxisLabel ?? undefined }}
 >
